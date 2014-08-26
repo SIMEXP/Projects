66e360c34a6e15eb2ef060a7a134821c75cc132d
yassinebha <yanamarji@gmail.com>
2014-08-26 16:38:12 -0400

    simu1c work in progress,  Purpuse: # 1-Test the stability of hertability estimate by decreasing the number of subjects until reaching # critical point where the script fail to retrive heritability from the simulated data. # 2-define the critcal mz/dz ration for different sampe size

---
+++ twins/hack_script/twins_heritability_simu1c.R
@@ -0,0 +1,171 @@
+#----------------------------------------------------------------------------------------------------------------------
+#                                                             Twin_simulate
+# Based on script in OpenMXUserGuide, p 15.
+# Purpuse: 
+# 1-Test the stability of hertability estimate by decreasing the number of subjects until reaching 
+# critical point where the script fail to retrive heritability from the simulated data.
+# 2-define the critcal mz/dz ration for different sampe size 
+#----------------------------------------------------------------------------------------------------------------------
+
+# ########################################## simulated dataset #########################################
+rm(list=ls())
+require(OpenMx)   # not needed for the simulation, but will be needed when we come to model specification
+require(MASS)    # needed for multivariate random number generation
+
+mya2<-0.5 #Additive genetic variance component (a squared)
+myc2<-0.3 #Common environment variance component (c squared)
+mye2<-1-mya2-myc2 #Specific environment variance component (e squared)
+
+my_rMZ <-mya2+myc2          # correlation between MZ twin1 and twin2
+my_rDZ <- .5*mya2+myc2     # correlation between DZ twin 1 and twin 2
+nb_subj <- 100 # number of total subjects
+step_seq <- 10 # step sequence to encrement the nuber of subjects
+percentil <- c(.25, .50, .75) # the sliding ratio MZ/DZ
+
+for (nSub in seq(step_seq,nb_subj,10)){
+  seq_MzDz=as.integer(quantile(seq(nSub), percentil))
+  for (nMz in seq_MzDz){
+    nb_Mz = nMz
+    nb_Dz = nSub - nMz
+    
+    set.seed(200)        # specified seed ensures same random number set generated on each run
+    mzData <- mvrnorm (nb_Mz, c(0,0), matrix(c(1,my_rMZ,my_rMZ,1),2,2))
+    set.seed(100)        # specified seed ensures same random number set generated on each run
+    dzData <- mvrnorm (nb_Dz, c(0,0), matrix(c(1,my_rDZ,my_rDZ,1),2,2))
+     
+    colnames(mzData) <- c('twin1', 'twin2') # assign column names
+    colnames(dzData) <- c('twin1', 'twin2')
+#     summary(mzData)
+#     summary(dzData)
+#     colMeans(mzData,na.rm=TRUE) #na.rm means ignore NA values (non-numeric)
+#     colMeans(dzData,na.rm=TRUE) 
+#     cov(mzData,use="complete") # "complete" specifies use only cases with data in all columns
+#     cov(dzData,use="complete") 
+#     alltwin=cbind(mzData,dzData)
+#     colnames(alltwin)=c("MZ_twin1","MZ_twin2","DZ_twin1","DZ_twin2")
+    # write.table(alltwin,"mytwinfile")     # Saves a copy of mydata in your R directory under name "mytwinfile"
+    selVars <- c('twin1','twin2')
+    
+    twinACEModel <- mxModel("twinACE",
+                            mxModel("ACE",
+                                    # Matrices a, c, and e to store a, c, and e path coefficients
+                                    mxMatrix(
+                                      type="Lower",
+                                      nrow=1,
+                                      ncol=1,
+                                      free=TRUE,
+                                      values=0.6,
+                                      labels="a11",
+                                      name="a"
+                                    ),
+                                    mxMatrix(
+                                      type="Lower",
+                                      nrow=1,
+                                      ncol=1,
+                                      free=TRUE,
+                                      values=0.6,
+                                      labels="c11",
+                                      name="c"
+                                    ),
+                                    mxMatrix(
+                                      type="Lower",
+                                      nrow=1,
+                                      ncol=1,
+                                      free=TRUE,
+                                      values=0.6,
+                                      labels="e11",
+                                      name="e"
+                                    ),
+                                    # Matrices A, C, and E compute variance components
+                                    mxAlgebra(
+                                      expression=a %*% t(a),
+                                      name="A"
+                                    ),
+                                    mxAlgebra(
+                                      expression=c %*% t(c),
+                                      name="C"
+                                    ),
+                                    mxAlgebra(
+                                      expression=e %*% t(e),
+                                      name="E"
+                                    ),
+                                    # Matrix & Algebra for expected means vector
+                                    mxMatrix(
+                                      type="Full",
+                                      nrow=1,
+                                      ncol=1,
+                                      free=TRUE,
+                                      values=20,
+                                      label="mean",
+                                      name="Mean"
+                                    ),
+                                    mxAlgebra(
+                                      expression= cbind(Mean,Mean),
+                                      name="expMean"
+                                    ),
+                                    # Algebra for expected variance/covariance matrix in MZ
+                                    mxAlgebra(
+                                      expression=rbind (cbind(A + C + E , A + C),
+                                                        cbind(A + C     , A + C + E)),
+                                      name="expCovMZ"
+                                    ),
+                                    # Algebra for expected variance/covariance matrix in DZ
+                                    mxAlgebra(
+                                      expression=rbind (cbind(A + C + E     , 0.5 %x% A + C),
+                                                        cbind(0.5 %x% A + C , A + C + E)),
+                                      name="expCovDZ"
+                                    )
+                            ),
+                            mxModel("MZ",
+                                    mxData(
+                                      observed=mzData,
+                                      type="raw"
+                                    ),
+                                    mxFIMLObjective(
+                                      covariance="ACE.expCovMZ",
+                                      means="ACE.expMean",
+                                      dimnames=selVars
+                                    )
+                            ),
+                            mxModel("DZ",
+                                    mxData(
+                                      observed=dzData,
+                                      type="raw"
+                                    ),
+                                    mxFIMLObjective(
+                                      covariance="ACE.expCovDZ",
+                                      means="ACE.expMean",
+                                      dimnames=selVars
+                                    )
+                            ),
+                            mxAlgebra(
+                              expression=MZ.objective + DZ.objective,
+                              name="minus2loglikelihood"
+                            ),
+                            mxAlgebraObjective("minus2loglikelihood")
+    )
+    twinACEFit <- mxRun(twinACEModel)
+    MZc <- mxEval(ACE.expCovMZ, twinACEFit)
+    DZc <- mxEval(ACE.expCovDZ, twinACEFit)
+    M <- mxEval(ACE.expMean, twinACEFit)
+    A <- mxEval(ACE.A, twinACEFit)
+    C <- mxEval(ACE.C, twinACEFit)
+    E <- mxEval(ACE.E, twinACEFit)
+    V <- (A+C+E)
+    a2 <- A/V
+    c2 <- C/V
+    e2 <- E/V
+    ACEest <- rbind(cbind(A,C,E),cbind(a2,c2,e2))
+    LL_ACE <- mxEval(objective, twinACEFit) 
+    # store the result in a tale 
+    if (nMz == seq_Mz[1]) {
+      TabResult <- matrix(, nrow = 1, ncol = 6) # empty matrix to hold results for each fir times point
+      colnames(TabResult) <- cbind("a2","c2","e2","LL_ACE","covMz","covDz")
+      TabResult <- data.frame(TabResult)
+      TabResult[1,] <- cbind(a2,c2,e2,LL_ACE,covMz[1,2],covDz[1,2])
+    } else {
+      TabResult[pp,] <- cbind(a2,c2,e2,LL_ACE,covMz[1,2],covDz[1,2])
+    }
+    
+  }
+}
