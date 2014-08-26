d1f6842931263894edb3de202d5280d415dccbbf
yassinebha <yanamarji@gmail.com>
2014-08-26 01:31:15 -0400

    simu1a

--- twins_heritability_simu1a.R
+++ twins_heritability_simu1a.R
@@ -163,17 +163,140 @@ LL_ACE <- mxEval(objective, twinACEFit)
 
 # ########################################## Real dataset #########################################
 rm(list=ls())
+require(OpenMx)   # not needed for the simulation, but will be needed when we come to model specification
+require(MASS)    # needed for multivariate random number generation
 
 data(twinData) # Australian data on body mass index (BMI) 
 summary(twinData)
 
-selVars <- c('bmi1','bmi2')
-mzData <- as.matrix(subset(twinData, zyg==1, c(bmi1,bmi2)))
-dzData <- as.matrix(subset(twinData, zyg==3, c(bmi1,bmi2)))
+selVars <- c('ht1','ht2')
+mzData <- as.matrix(subset(twinData, zyg==1, c(ht1,ht2)))
+dzData <- as.matrix(subset(twinData, zyg==3, c(ht1,ht2)))
 colMeans(mzData,na.rm=TRUE)
 colMeans(dzData,na.rm=TRUE)
 cov(mzData,use="complete")
 cov(dzData,use="complete")
+
+# do scatterplots for MZ and DZ
+split.screen(c(1,2))        # split display into two screens side by side
+# (use c(2,1) for screens one above the other)
+screen(1)
+plot(mzData,main='MZ')    # main specifies overall plot title
+
+screen(2)
+plot(dzData, main='DZ')
+
+twinACEModel <- mxModel("twinACE",
+                        mxModel("ACE",
+                                # Matrices a, c, and e to store a, c, and e path coefficients
+                                mxMatrix(
+                                  type="Lower",
+                                  nrow=1,
+                                  ncol=1,
+                                  free=TRUE,
+                                  values=0.6,
+                                  labels="a11",
+                                  name="a"
+                                ),
+                                mxMatrix(
+                                  type="Lower",
+                                  nrow=1,
+                                  ncol=1,
+                                  free=TRUE,
+                                  values=0.6,
+                                  labels="c11",
+                                  name="c"
+                                ),
+                                mxMatrix(
+                                  type="Lower",
+                                  nrow=1,
+                                  ncol=1,
+                                  free=TRUE,
+                                  values=0.6,
+                                  labels="e11",
+                                  name="e"
+                                ),
+                                # Matrices A, C, and E compute variance components
+                                mxAlgebra(
+                                  expression=a %*% t(a),
+                                  name="A"
+                                ),
+                                mxAlgebra(
+                                  expression=c %*% t(c),
+                                  name="C"
+                                ),
+                                mxAlgebra(
+                                  expression=e %*% t(e),
+                                  name="E"
+                                ),
+                                # Matrix & Algebra for expected means vector
+                                mxMatrix(
+                                  type="Full",
+                                  nrow=1,
+                                  ncol=1,
+                                  free=TRUE,
+                                  values=20,
+                                  label="mean",
+                                  name="Mean"
+                                ),
+                                mxAlgebra(
+                                  expression= cbind(Mean,Mean),
+                                  name="expMean"
+                                ),
+                                # Algebra for expected variance/covariance matrix in MZ
+                                mxAlgebra(
+                                  expression=rbind (cbind(A + C + E , A + C),
+                                                    cbind(A + C     , A + C + E)),
+                                  name="expCovMZ"
+                                ),
+                                # Algebra for expected variance/covariance matrix in DZ
+                                mxAlgebra(
+                                  expression=rbind (cbind(A + C + E     , 0.5 %x% A + C),
+                                                    cbind(0.5 %x% A + C , A + C + E)),
+                                  name="expCovDZ"
+                                )
+                        ),
+                        mxModel("MZ",
+                                mxData(
+                                  observed=mzData,
+                                  type="raw"
+                                ),
+                                mxFIMLObjective(
+                                  covariance="ACE.expCovMZ",
+                                  means="ACE.expMean",
+                                  dimnames=selVars
+                                )
+                        ),
+                        mxModel("DZ",
+                                mxData(
+                                  observed=dzData,
+                                  type="raw"
+                                ),
+                                mxFIMLObjective(
+                                  covariance="ACE.expCovDZ",
+                                  means="ACE.expMean",
+                                  dimnames=selVars
+                                )
+                        ),
+                        mxAlgebra(
+                          expression=MZ.objective + DZ.objective,
+                          name="minus2loglikelihood"
+                        ),
+                        mxAlgebraObjective("minus2loglikelihood")
+)
+twinACEFit <- mxRun(twinACEModel)
+MZc <- mxEval(ACE.expCovMZ, twinACEFit)
+DZc <- mxEval(ACE.expCovDZ, twinACEFit)
+M <- mxEval(ACE.expMean, twinACEFit)
+A <- mxEval(ACE.A, twinACEFit)
+C <- mxEval(ACE.C, twinACEFit)
+E <- mxEval(ACE.E, twinACEFit)
+V <- (A+C+E)
+a2 <- A/V
+c2 <- C/V
+e2 <- E/V
+ACEest <- rbind(cbind(A,C,E),cbind(a2,c2,e2))
+LL_ACE <- mxEval(objective, twinACEFit)
 
 
 # #Run AE model
