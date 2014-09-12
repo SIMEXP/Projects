563d4e651c19714279ad07c59467995ce9648aa1
Pierre Bellec <pierre.bellec@gmail.com>
2014-09-12 17:11:43 -0400

    Short demo to extract maximal absolute effects in a summary table, from the results of GLM-connectome.

---
+++ Misc/niakmisc_demo_extract_max_effects.m
@@ -0,0 +1,46 @@
+clear
+
+%% This script shows how to extract the beta values and the significance for a number of connections
+%% from the results of the glm_connectome pipeline.
+%%
+%% This script will download and extract some data in the current folder, if it can't find a number of datasets.
+%% It will also generate a number of figures and volumes
+%% Please execute in a dedicated folder
+%% If the script is executed multiple times, it is supposed to download the data only once. 
+%% Erase the content of the folder to start from scratch.
+
+%% Download example time series
+if ~psom_exist('cobre_glm_connectome_nii')
+    system('wget http://www.nitrc.org/frs/download.php/6814/cobre_glm_connectome_nii.zip')
+    system('unzip cobre_glm_connectome_nii.zip')
+    psom_clean('cobre_glm_connectome_nii.zip')
+end     
+
+%% Load the effect maps and corresponding networks
+path_ref  = [pwd filesep];
+path_net  = [path_ref 'cobre_glm_connectome_nii' filesep 'sci10_scg10_scf10' filesep];
+path_maps = [path_net 'szVScont_age_sex_FD' filesep];
+file_eff  = [path_maps 'effect_szVScont_age_sex_FD_sci10_scg10_scf10.nii.gz'];
+[hdr,eff] = niak_read_vol(file_eff);
+file_net  = [path_net 'networks_sci10_scg10_scf10.nii.gz'];
+[hdr,net] = niak_read_vol(file_net);
+
+%% Extract max absolute effects
+nb_net = max(net(:));
+val_max = zeros(nb_net,1);
+ind_max = zeros(nb_net,1);
+for nn = 1:max(net(:))
+    map_eff_net = eff(:,:,:,nn);
+    [val,ind] = max(abs(map_eff_net(:)));
+    val = val(1);
+    ind = ind(1);
+    ind_max(nn) = net(ind);
+    val_max(nn) = val;
+end
+
+%% Generate a .csv summary
+[val_max,order] = sort(val_max,'descend');
+tab = [order , val_max , ind_max(order)];
+
+opt_csv.labels_y = {'network index' , 'max abs effect' , 'with network index'};
+niak_write_csv([path_ref 'summary_max_eff.csv'],tab,opt_csv);
