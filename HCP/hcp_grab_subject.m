8a268f89b132793c0689a81a7e97031f06b5735a
yassinebha <yanamarji@gmail.com>
2014-11-06 00:15:07 -0500

    up

--- HCP/hcp_grab_subject.m
+++ HCP/hcp_grab_subject.m
@@ -4,9 +4,10 @@ for nn = 1:4
     path_data = ['/media/S500-' num2str(nn) '-20140805/'];
     opt.path_out = '/media/database1/';
     opt.type_task = 'EMOTION';
-    niak_extract_preprocessed_hcp(path_data,opt);
+    files = niak_extract_preprocessed_hcp(path_data,opt);
 end
 
 opt.path_out = '~/Desktop/';
 opt.type_task = 'EMOTION';
 path_data = '/media/database1/hcp/';
+files = niak_extract_preprocessed_hcp(path_data,opt);
