8321ba3d13ab7d58783c265bb75cbb55365e26f3
yassinebha <yanamarji@gmail.com>
2014-11-18 01:49:46 -0500

    up

--- HCP/hcp_pipeline_stability_fir.m
+++ HCP/hcp_pipeline_stability_fir.m
@@ -30,7 +30,7 @@ clear all
 %% Parameters
 %%%%%%%%%%%%%%%%%%%%%
 task  = 'emotion';
-exp   = 'hcp';
+exp   = 'niak';
 fprintf ('script to run niak_stability_fir pipeline \n Task: %s \n experiment: %s\n',task,exp)
 %% Setting input/output files 
 [status,cmdout] = system ('uname -n');
