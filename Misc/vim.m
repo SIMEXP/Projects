306dce571d8986d706625cb7d595756f92ded776
yassinebha <yanamarji@gmail.com>
2014-12-13 02:19:51 -0500

    optimization

--- Misc/vim.m
+++ Misc/vim.m
@@ -2,17 +2,11 @@
 function [status,output] = vim(func_name);
 file_name  = which(func_name);
 file_name  = strrep(file_name,' ','\ ');
-terminal   = sprintf('echo $TERM');
 envirement = sprintf ('echo $TMUX');
-[out_term, text_term] = system (terminal);
 [out_env, text_env] = system (envirement);
 IDX_env  = strfind (text_env,'tmux');
-IDX_term = strfind (text_term,'xterm');
-if isempty(IDX_term)
-   if isempty (IDX_env)
-      command = sprintf('gnome-terminal -e "vim -f %s" &',file_name);
+if isempty (IDX_env)
+   command = sprintf('gnome-terminal -e "vim -f %s" &',file_name);
    else command = sprintf('tmux splitw -v "vim %s"',file_name); 
-   end
-else command = sprintf('xterm -e "vim -f %s" &',file_name);
 end
 system(command,0,'async');
