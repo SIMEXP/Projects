bb46fae42b5c224505d9b7a0a7e1f793491ed956
yassinebha <yanamarji@gmail.com>
2014-12-28 01:54:34 -0500

    function that create model for breath hold task

---
+++ NKI_enhanced/nki_model_breathhold.m
@@ -0,0 +1,77 @@
+function [] = nki_model_breathhold(path_folder,opt)
+% generate individual model for motor task HCP database.
+%
+% SYNTAX:
+% [] = HCP_IND_MODEL_BREATHHOLD(OPT)
+%
+% _________________________________________________________________________
+% PATH_FOLDER (string, default [pwd]) the full path to the root folder that contain the 
+%   individual NKI task model variables. 
+%
+% OPT
+%   (structure, optional) with the following fields :
+%
+%   TASK (string, default 'breathhold') type of trial that would be extracted, possiblr trial : 'checkerboard', 'breathhold'.
+%   TRIAL_DELAY (numeric, default '1,5') time delay before each trial to estimate FIR responses.
+%   TRIAL_DURATION (numeric, default '16.5') time for each trial to estimate FIR responses.
+%   BASELINE_DELAY (numeric, default '2.5') time delay before each trial's baseline estimate.
+%   BASELINE_DURATION (numeric, default '4') time for each trial to estimate baselie for FIR responses.
+%   EXP (string, default '1400') type of TR used. Possible value '1400', '645'.
+
+%%%%%%%%%%%%%%%%%%%%%
+%% Parameters
+%%%%%%%%%%%%%%%%%%%%%
+
+path_folder = niak_full_path (path_folder);
+
+%% Default options
+list_fields   = { 'task'       , 'trial_delay' , 'trial_duration' , 'baseline_delay' , 'baseline_duration' , 'exp'  };
+list_defaults = { 'breathhold' , 1.5           ,  30.5              ,  2.5             ,  4                  , '1400' };
+if (nargin > 1) && ~isempty(opt.task) 
+    opt = psom_struct_defaults(opt,list_fields,list_defaults);
+else
+    opt = psom_struct_defaults(struct(),list_fields,list_defaults);
+end
+
+%% path and files names
+data.dir_output         = path_folder;
+data.name_csv_intrarun  = ['nki_model_intrarun_' lower(opt.task)];
+
+%% intrarun fir
+opt_csv_read.separator= sprintf(',');
+data.covariates_intrarun_names = {'times','duration'};
+data.covariates_intrarun_cond  = {'baseline',opt.task,'baseline',opt.task,'baseline',opt.task,'baseline',opt.task,'baseline',opt.task,'baseline',opt.task,'baseline',opt.task};
+data.covariates_intrarun_values(1,1) = (0 - opt.baseline_delay) + 10;
+data.covariates_intrarun_values(1,2) = opt.baseline_duration;
+data.covariates_intrarun_values(2,1) = (10 - opt.trial_delay);
+data.covariates_intrarun_values(2,2) = opt.trial_duration;
+data.covariates_intrarun_values(3,1) = (36 - opt.baseline_delay) + 10;
+data.covariates_intrarun_values(3,2) = opt.baseline_duration;
+data.covariates_intrarun_values(4,1) = (46 - opt.trial_delay);
+data.covariates_intrarun_values(4,2) = opt.trial_duration;
+data.covariates_intrarun_values(5,1) = (72 - opt.baseline_delay) + 10;
+data.covariates_intrarun_values(5,2) = opt.baseline_duration;
+data.covariates_intrarun_values(6,1) = (82 - opt.trial_delay);
+data.covariates_intrarun_values(6,2) = opt.trial_duration;
+data.covariates_intrarun_values(7,1) = (108 - opt.baseline_delay) + 10;
+data.covariates_intrarun_values(7,2) = opt.baseline_duration;
+data.covariates_intrarun_values(8,1) = (118 - opt.trial_delay);
+data.covariates_intrarun_values(8,2) = opt.trial_duration;
+data.covariates_intrarun_values(9,1) = (144 - opt.baseline_delay) + 10;
+data.covariates_intrarun_values(9,2) = opt.baseline_duration;
+data.covariates_intrarun_values(10,1) = (154 - opt.trial_delay);
+data.covariates_intrarun_values(10,2) = opt.trial_duration;
+data.covariates_intrarun_values(11,1) = (180 - opt.baseline_delay) + 10;
+data.covariates_intrarun_values(11,2) = opt.baseline_duration;
+data.covariates_intrarun_values(12,1) = (190 - opt.trial_delay);
+data.covariates_intrarun_values(12,2) = opt.trial_duration;
+data.covariates_intrarun_values(13,1) = (216 - opt.baseline_delay) + 10;
+data.covariates_intrarun_values(13,2) = opt.baseline_duration;
+data.covariates_intrarun_values(14,1) = (226 - opt.trial_delay);
+data.covariates_intrarun_values(14,2) = opt.trial_duration;
+
+opt_csv_write.labels_y = data.covariates_intrarun_names;
+opt_csv_write.labels_x = data.covariates_intrarun_cond;
+opt_csv_write.precision = 2;
+niak_write_csv(strcat(data.dir_output,data.name_csv_intrarun,'.csv'),data.covariates_intrarun_values,opt_csv_write);
+
