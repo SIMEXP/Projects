daf57e6cb2280937bed6b4679da61e5d418d7bd5
Pierre Bellec <pierre.bellec@gmail.com>
2015-01-28 22:18:42 -0500

    Added some correction for multiple comparisons across variables.

--- preventad/preventad_xp_2015_01_28.m
+++ preventad/preventad_xp_2015_01_28.m
@@ -1,6 +1,6 @@
 clear
 
-num_net = 8;
+num_net = 6;
 path_data = '/home/pbellec/database/preventad/scores_2015_01_28/';
 path_res = [path_data 'cluster_' num2str(num_net) 'R_diff' filesep];
 
@@ -22,7 +22,8 @@ order = niak_part2order (part,R);
 
 %% Visualize the matrices
 figure
-opt_vr.limits = [-0.5 0.5];
+%opt_vr.limits = [-0.5 0.5];
+opt_vr.limits = [-0.3 0.3];
 niak_visu_matrix(R(order,order),opt_vr);
 figure
 opt_p.flag_labels = true;
@@ -39,17 +40,21 @@ end
 %% Load phenotypic variables
 [tab,lx,ly] = niak_read_csv([path_data 'model_preventad_20141215.csv']);
 load([path_data 'list_subject.mat']);
-tab2 = zeros(length(list_subject,size(tab,2)));
+tab2 = zeros(length(list_subject),size(tab,2));
 for ss = 1:length(list_subject)
     ind_s = find(ismember(lx,list_subject{ss}));
     tab2(ss,:) = tab(ss,:);
 end
 
 %% GLM analysis 
+list_cov = [5 9 11 13];
 %list_cov = [1 2 5 9 13];
-list_cov = [5];
-for cc = list_cov
-    covar = tab2(:,cc);
+%list_cov = [5];
+%list_cov = 1:14;
+%list_cov = [5 6 7 8 9 11 12 13];
+pce = zeros(length(list_cov),max(part));
+for cc = 1:length(list_cov)
+    covar = tab2(:,list_cov(cc));
     fd = tab2(:,15);
     mask_covar = ~isnan(covar);
     %mask_covar = ~isnan(covar)&(tab2(:,2)~=1);
@@ -59,10 +64,15 @@ for cc = list_cov
     opt_glm.test = 'ttest';
     opt_glm.flag_beta = true;
     res_covar = niak_glm(model_covar,opt_glm);
-    fprintf('%s\n',ly{cc});
+    fprintf('%s\n',ly{list_cov(cc)});
     %res_covar.beta(model_covar.c>0,:)
-    res_covar.pce
+    pce(cc,:) = res_covar.pce;
 end
+[fdr,test] = niak_fdr(pce(:),'BH',0.05);
+pce
+test = reshape(test,size(pce))
+
+if 0
 
 w_hat = model_covar.x*res_covar.beta;
 plot(w_hat(:,1), model_covar.y(:,1),'.')
@@ -80,23 +90,6 @@ res_covar = niak_glm(model_covar,opt_glm);
 res_covar.pce;
 res_covar.beta(model_covar.c>0,:);
 w_hat = model_covar.x*res_covar.beta;
-plot(w_hat(:,2), model_covar.y(:,2),'.')
-
-%% GLM analysis 
-covar = tab2(:,9);
-gender = tab2(:,2);
-fd = tab2(:,15); 
-mask_covar = ~isnan(covar)&(gender==0);
-model_covar.x = [ones(sum(mask_covar),1) niak_normalize_tseries([covar(mask_covar) fd(mask_covar)],'mean')];
-model_covar.y = weights(mask_covar,:);
-model_covar.c = [0 ; 1 ; 0];
-opt_glm.test = 'ttest';
-opt_glm.flag_beta = true;
-res_covar = niak_glm(model_covar,opt_glm);
-res_covar.beta(model_covar.c>0,:)
-res_covar.pce
-w_hat = model_covar.x*res_covar.beta;
-figure
 plot(w_hat(:,2), model_covar.y(:,2),'.')
 
 %% GLM analysis the other way
@@ -160,3 +153,4 @@ niak_write_vol(hdr,vol_avg);
 hdr.file_name = [path_res 'grand_mean_clusters.nii.gz'];
 niak_write_vol(hdr,mean(stab,4));
 
+end
