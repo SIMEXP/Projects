fc26560e5318da6413130a6fab5da4f78a90023f
Pierre Bellec <pierre.bellec@gmail.com>
2015-02-10 22:56:05 -0500

    New parameters, faster to run.

--- states/states_xp_2015_02_10.m
+++ states/states_xp_2015_02_10.m
@@ -4,7 +4,7 @@ clear
 %% Simulation parameters
 
 % The number of regions
-N = 90; 
+N = 30; 
 % The number of clusters
 nb_clust = 3;
 size_clust = N/nb_clust;
@@ -17,9 +17,9 @@ states2 = [ false(35,1) ; true(15,1) ; false(35,1) ; true(15,1) ; false(35,1) ; 
 sigma_inter = [0 1]; % First number drives the connectivity between clusters 1 and 2 at state 0, and the second number for state 1.
 sigma_intra = 1;     % strength of within-cluster connectivity
 % Number of subjects per group
-S = 20;
+S = 10;
 % Window length
-length_w = 20;
+length_w = 15;
 % The number of tested states
 nb_clust_states = 3;
 
@@ -50,10 +50,22 @@ conn = states_dyn_conn(y,length_w);
 % niak_visu_motion(conn{1}) % Subject in group 1
 % niak_visu_motion(conn{21}) % Subject in group 2
 
-% vectorize connectomes
+% vectorize connectomes, as well as associated subject and state information
 all_conn = [];
+ind_subj = [];
+ind_states = [];
 for ss = 1:2*S
     all_conn = [ all_conn conn{ss}];
+    ind_subj = [ ind_subj ; ss*ones(size(conn{ss},2),1)];
+    perc_states = zeros(size(conn{ss},2),1);
+    for ww = 1:size(conn{ss},2)
+        if ss<=S
+            perc_states(ww) = mean(states1(ww:(ww+length_w-1)));
+        else
+            perc_states(ww) = mean(states2(ww:(ww+length_w-1)));
+        end
+    end
+    ind_states = [ind_states ; perc_states];
 end
 % At this stage we end up with a 4005 (connections in the connectome) x 7640 (number of time windows x number of subjects) array
 % Let's build a (correlation-based) distance matrix between dynamic connectomes
@@ -67,7 +79,16 @@ hier = niak_hierarchical_clustering(R);
 part = niak_threshold_hierarchy(hier,struct('thresh',nb_clust_states));
 for cc = 1:nb_clust_states
     avg_states(:,cc) = mean(all_conn(:,part==cc),2);
+    demeaned_states(:,cc) = mean(all_conn(:,part==cc),2) - mean(all_conn,2);
 end
-% For (intersting) fun again, let's visualize the estimated states
+% For (interesting) fun again, let's visualize the estimated states
 % niak_visu_matrix(avg_states(:,1))
 % There is clearly one that corresponds to state 1, another to state 0, and one in between, as expected
+
+
+%% Now, on to the statistics!
+
+% Start building weights
+all_demeaned = niak_normalize_tseries(all_conn','mean')';
+weights = corr(all_demeaned,demeaned_states);
+
