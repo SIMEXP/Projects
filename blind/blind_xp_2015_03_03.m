f4d69df221ade6bbad183d0bd1d725601b883ddb
Pierre Bellec <pierre.bellec@gmail.com>
2015-03-03 20:53:10 -0500

    New experiments on the blind dataset.

--- blind/blind_xp_2015_03_03.m
+++ blind/blind_xp_2015_03_03.m
@@ -1,12 +1,16 @@
 clear all
 
 path_data = '/home/pbellec/database/blindtvr/xp_2015_03/';
-% label_contrast = 'rest_CBvsSC';
-% label_contrast = 'tvr_CBvsSC';
-label_contrast = 'task_CBvsSC';
+%label_contrast = 'rest_CBvsSC';
+label_contrast = 'tvr_CBvsSC';
+%label_contrast = 'task_CBvsSC';
+
+%seeds = [74 75 140];
+seeds = 67;
 
 %% Load partitions
-[hdr,sc159] = niak_read_vol([path_data 'networks_sci180_scg162_scf159.nii.gz']);
+%[hdr,sc159] = niak_read_vol([path_data 'networks_sci180_scg162_scf159.nii.gz']);
+[hdr,sc159] = niak_read_vol([path_data 'networks_sci95_scg86_scf86.nii.gz']);
 [hdr,sc8] = niak_read_vol([path_data 'networks_sci11_scg8_scf8.nii.gz']);
 mask = sc159>0;
 part159 = sc159(mask);
@@ -20,11 +24,5 @@ end
 
 %% Load GLM
 glm = load([path_data label_contrast '/glm_' label_contrast '_sci180_scg162_scf159.mat']);
-model = glm.model_group;
-opt_test.q = 0.1;
-opt_test.q_omni = 0.05;
-opt_test.method = 'TST';
-opt_test.nb_samps = 10000;
-res = niak_network_fdr(model,part,opt_test);
-hdr.file_name = [path_data label_contrast '/perc_disc_network_fdr.nii.gz'];
-niak_write_vol(hdr,niak_part2vol(mean(niak_lvec2mat(res.test_fdr{1}),1),sc159));
+pce = niak_lvec2mat(glm.pce);
+pce = pce(part == 8, seeds);
