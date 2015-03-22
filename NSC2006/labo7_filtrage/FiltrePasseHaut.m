88be3443835f0f44ceb6c578535adb13019b4121
Pierre Bellec <pierre.bellec@gmail.com>
2015-03-22 15:32:03 -0400

    The candidate version of the lab on filtering.

--- NSC2006/labo7_filtrage/FiltrePasseHaut.m
+++ NSC2006/labo7_filtrage/FiltrePasseHaut.m
@@ -3,7 +3,7 @@ function [sig_filt]=FiltrePasseHaut(signal,fs,freq_coupure)
 % signal. freq_coupure est la fréquence de coupure du filtre passe-haut
 
 dfiltfreq=freq_coupure/(fs/2);      % calcule la fréquence de coupure normalisée
-[b,a]=cheby1(8,dfiltfreq,'high');   % crée un filtre passe-haut
+[b,a]=butter(8,dfiltfreq,'high');   % crée un filtre passe-haut
 sig_filt=filtfilt(b,a,signal);      % applique le filtre au signal
-toto
+
 return
