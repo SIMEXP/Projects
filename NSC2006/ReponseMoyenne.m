10e0993eb7bc160bad954aad8e66f5e625cd2f4d
Pierre Bellec <pierre.bellec@gmail.com>
2015-04-06 23:53:04 -0400

    Updated functions for NSC2006.
    xy#

--- NSC2006/ReponseMoyenne.m
+++ NSC2006/ReponseMoyenne.m
@@ -1,16 +1,21 @@
 function moy = ReponseMoyenne(taille)
-% Cette fonction génère une fonction de réponse 
-% telle que la convolution corresponde à une moyenne mobile
+% Cette fonction génère une fonction de réponse à une impulsion,
+% correspondant à une moyenne mobile
+%
 % Syntaxe: moy = ReponseMoyenne(taille)
-% taille: (entier) le nombre de points dans la moyenne
-% moy: le vecteur associé a la moyenne mobile
+%  taille: (entier) le nombre de points dans la moyenne
+%  moy: (vecteur) réponse associée a la moyenne mobile
+%
+% Remarque:
+%  Le code de cette fonction a été rencontré à la question 1.2 
+%  du laboratoire du cours 6 (introduction au traitement  des signaux)
 %
 % Exemple:
-% moy = ReponseMoyenne(4);
-% stem(moy)
+%  moy = ReponseMoyenne(4);
+%  plot(moy,'bo')
 
-if ~isnumeric(taille)||(numel(taille)>1)||(round(taille)~=taille)
-    error('Le paramétre taille doit être un nombre entier')
-end
+% On commence par une réponse composée de 1
 moy = ones(1,taille);
-moy = moy/sum(moy(:));
+
+% On divise par le nombre de points pour avoir une moyenne
+moy = moy/taille;
