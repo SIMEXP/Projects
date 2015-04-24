45af479fb9d36c59ebcfed906e7f1e97a2718fd6
surchs <sebastian.urchs@gmail.com>
2015-04-24 15:09:11 -0400

    mouse better

--- Scores/Simulation/mouse.m
+++ Scores/Simulation/mouse.m
@@ -1,4 +1,4 @@
-
+clear all; close all;
 edge = 64;
 n_edge = edge/4;
 
@@ -9,10 +9,10 @@ y = 16;
 left_ear = sqrt((rr-n_edge).^2+(cc-n_edge).^2)<=n_edge;
 right_ear = sqrt((rr-n_edge*3).^2+(cc-n_edge).^2)<=n_edge;
 face = sqrt((rr-n_edge*2).^2+(cc-n_edge*2.5).^2)<=n_edge*1.4;
-left_eye = sqrt((rr-n_edge*1.5).^2+(cc-n_edge*2).^2)<=6;
-right_eye = sqrt((rr-n_edge*2.5).^2+(cc-n_edge*2).^2)<=6;
+left_eye = sqrt((rr-n_edge*1.5).^2+(cc-n_edge*2).^2)<=n_edge/2.5;
+right_eye = sqrt((rr-n_edge*2.5).^2+(cc-n_edge*2).^2)<=n_edge/2.5;
 mouth = zeros(edge,edge);
-mouth(n_edge*3:n_edge*3+4,n_edge*1.5:n_edge*2.5) = 1;
+mouth(n_edge*3:n_edge*3+n_edge/4,n_edge*1.5:n_edge*2.5) = 1;
 mousy = logical(left_ear + right_ear + face) - left_eye - right_eye - mouth;
 %C = logical(C);
 mouse_mask = logical(mousy);
