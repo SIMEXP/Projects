b94fd22aba88245bd5546e73e115a2e253e55d7e
surchs <sebastian.urchs@gmail.com>
2015-05-20 19:06:42 -0400

    changes to the testing script. should work

--- Scores/Test/Scripts/test_stability_pipeline.m
+++ Scores/Test/Scripts/test_stability_pipeline.m
@@ -11,17 +11,20 @@ opt.min_nb_vol = 10;
 in_data = niak_grab_fmri_preprocess(fmri_data, opt);
 
 %% Get the cambridge template
-data.url = 'http://files.figshare.com/1861820/template_cambridge_basc_multiscale_nii_asym.zip';
-data.name = 'template_cambridge_basc_multiscale_nii_asym.zip';
+data.url = 'http://files.figshare.com/1861822/template_cambridge_basc_multiscale_mnc_asym.zip';
+data.name = 'template_cambridge_basc_multiscale_mnc_asym.zip';
 niak_wget(data);
 % Template folder
-template_data = [pwd filesep 'template_cambridge_basc_multiscale_nii_asym'];
-template_name = 'template_cambridge_basc_multiscale_asym_scale%03d.nii.gz';
+template_data = [pwd filesep 'template_cambridge_basc_multiscale_mnc_asym'];
+template_name = 'template_cambridge_basc_multiscale_asym_scale%03d.mnc.gz';
+
+%% Resample the template to the mask size
+scale = 7;
+system('mincresample -like target_test_niak_mnc1-2015-05-15/demoniak_preproc/quality_control/group_coregistration/func_mask_group_stereonl.mnc.gz template_cambridge_basc_multiscale_mnc_asym/template_cambridge_basc_multiscale_asym_scale007.mnc.gz template_cambridge_basc_multiscale_mnc_asym/supersmall_mask_007.mnc.gz');
 
 %% All present and accounted for, let's call the pipeline
 opt_scores = struct;
 opt_scores.folder_out = [pwd filesep 'pipe_out'];
-scale = 7;
-in_data.part = [template_data filesep sprintf(template_name, scale)];
+in_data.part = [template_data filesep 'supersmall_mask_007.mnc.gz'];
 %% Call the pipeline
 [pipeline, opt_scores] = niak_pipeline_stability_scores(in_data, opt_scores);
