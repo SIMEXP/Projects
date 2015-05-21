9eb4cda875a5006d5a25262556b40a914e740420
surchs <sebastian.urchs@gmail.com>
2015-05-21 15:02:53 -0400

    changing output name to conform with other pipelines

--- Scores/Test/Scripts/test_stability_pipeline.m
+++ Scores/Test/Scripts/test_stability_pipeline.m
@@ -20,11 +20,11 @@ template_name = 'template_cambridge_basc_multiscale_asym_scale%03d.mnc.gz';
 
 %% Resample the template to the mask size
 scale = 7;
-system('mincresample -like target_test_niak_mnc1-2015-05-15/demoniak_preproc/quality_control/group_coregistration/func_mask_group_stereonl.mnc.gz template_cambridge_basc_multiscale_mnc_asym/template_cambridge_basc_multiscale_asym_scale007.mnc.gz template_cambridge_basc_multiscale_mnc_asym/supersmall_mask_007.mnc.gz');
+system('mincresample -like target_test_niak_mnc1-2015-05-15/demoniak_preproc/quality_control/group_coregistration/func_mask_group_stereonl.mnc.gz template_cambridge_basc_multiscale_mnc_asym/template_cambridge_basc_multiscale_asym_scale007.mnc.gz template_cambridge_basc_multiscale_mnc_asym/supersmall_mask_007.mnc');
 
 %% All present and accounted for, let's call the pipeline
 opt_scores = struct;
-opt_scores.folder_out = [pwd filesep 'pipe_out'];
-in_data.part = [template_data filesep 'supersmall_mask_007.mnc.gz'];
+opt_scores.folder_out = [pwd filesep 'scores_demoniak'];
+in_data.part = [template_data filesep 'supersmall_mask_007.mnc'];
 %% Call the pipeline
 [pipeline, opt_scores] = niak_pipeline_stability_scores(in_data, opt_scores);
