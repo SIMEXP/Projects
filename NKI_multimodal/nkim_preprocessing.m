9f307b93b50a1521f6502e4994b6b0c1a953e0ab
AmanPreet Badhwar <amanpreet.badhwar@mail.mcgill.ca>
2015-06-05 00:47:29 -0400

    added background mode

--- NKI_multimodal/nkim_preprocessing.m
+++ NKI_multimodal/nkim_preprocessing.m
@@ -150,6 +150,7 @@ opt.smooth_vol.flag_skip = 0;  % Skip spatial smoothing (0: don't skip, 1 : skip
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Run the fmri_preprocess pipeline  %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+opt.psom.mode_pipeline_manager = 'background'; 
 opt.psom.qsub_options = '-q sw -l nodes=1:ppn=4,walltime=48:00:00';
 opt.granularity = 'subject'; 
 opt.psom.max_queued = 150;
