90f2737214cd63cd778e8547f291e028a8588d76
angela-tam <angela.tam08@gmail.com>
2015-07-09 13:27:53 -0400

    boxplot works now

--- adnet/adnet_box_scatter_plot.m
+++ adnet/adnet_box_scatter_plot.m
@@ -25,55 +25,31 @@ data_cne = {adni_cne,criugmmci_cne,adpd_cne,mnimci_cne};
 data_mci = {adni_mci,criugmmci_mci,adpd_mci,mnimci_mci};
 %data_all = {data_cne,data_mci};
 
-%% plotting raw data points for all sites for one connection
-for ii = 1:length(data_cne)
-    hold on
-    plot(ii-0.05+0.1*rand(size(data_cne{ii},1),1),data_cne{ii}(:,connec),'.','Marker','o','MarkerSize',3,'MarkerFaceColor','red','MarkerEdgeColor','red');
-    plot(ii+0.25+0.1*rand(size(data_mci{ii},1),1),data_mci{ii}(:,connec),'.','Marker','o','MarkerSize',3,'MarkerFaceColor','blue','MarkerEdgeColor','blue');
+
+all_data=[];
+labels_data=[];
+for i = 1:4
+    all_data = [all_data;data_cne{i}];
+    labels_data = [labels_data;repmat(10*(i),length(data_cne{i}),1)];
+    
+    all_data = [all_data;data_mci{i}]
+    labels_data = [labels_data;repmat(10*(i+0.5),length(data_mci{i}),1)];
 end
 
-%% making box plots
-for jj = 1:length(data_cne)
-%     maxpos = size(data_cne,2);
-    position_cne = linspace(1,jj,jj); % set position of cne boxes to be placed from 1 to length(data_cne)
-    position_mci = position_cne+0.3; % set position of mci boxes to be shifted from cne by 0.3
-    boxplot(data_cne{jj},'colors','k','width',0.18,'positions',position_cne,'symbol','');
-    set(gca,'XTickLabel',{' '}) % temporarily get rid of xtick labels
-    hold on
-    boxplot(data_mci{jj},'colors','k','width',0.18,'positions',position_mci,'symbol',''); 
+hold on
+%% plotting raw data points for all sites for one connection
+for ii = 1:4
+    idx = ii*2-1
+    plot(idx-0.05+0.1*rand(size(data_cne{ii},1),1),data_cne{ii}(:,connec),'.','Marker','o','MarkerSize',3,'MarkerFaceColor','red','MarkerEdgeColor','red');
 end
-    
-% %% box plot
-% 
-% 
-% adni_position_cne = 1;%:1:num_sig; % set position of cne boxes to be placed from 1 to num_sig
-% adni_position_mci = 1.3;%:1:num_sig+0.3; % set position of mci boxes to be shifted from cne by 0.3
-% boxplot(adni_cne,'colors','k','width',0.18,'positions',1,'symbol','');
-% hold on
-% boxplot(adni_mci,'colors','k','width',0.18,'positions',1.3,'symbol','');
-% hold on
-% criugmmci_position_cne = 2;
-% criugmmci_position_mci = criugmmci_position_cne+0.3;
-% boxplot(criugmmci_cne,'colors','b','width',0.18,'positions',2,'symbol','');
-% hold on
-% boxplot(criugmmci_mci,'colors','b','width',0.18,'positions',2.3,'symbol',''); 
-% 
-% criugmmci_position_cne = 1:1:2;
-% criugmmci_position_mci = criugmmci_position_cne+0.3;
-% boxplot([criugmmci_cne,criugmmci_mci],'colors','bk','width',0.18,'positions',criugmmci_position_cne,'symbol','');
-% hold on
-% boxplot([criugmmci_mci,criugmmci_mci],'colors','bk','width',0.18,'positions',criugmmci_position_mci,'symbol',''); 
-% 
-% yy = nan(size(adni_cne));
-% criugmmci_cne_b = yy(1:numel(criugmmci_cne));
-% boxplot([adni_cne,criugmmci_cne_b])
-% 
-% 
-% 
-% x = [data_cne,data_mci];
-% group = [1,2];
-% positions = [1,2];
-% boxplot(x,group,'positions',positions);
+
+for ii = 1:4
+    idx = ii*2
+    plot(idx-0.05+0.1*rand(size(data_mci{ii},1),1),data_mci{ii}(:,connec),'.','Marker','o','MarkerSize',3,'MarkerFaceColor','blue','MarkerEdgeColor','blue');
+end
+
+%% overlay box plots
+boxplot(all_data,labels_data,'color','k','symbol','','width',0.18);
 
 %% aesthetics
 ylim([-1 1.5])
