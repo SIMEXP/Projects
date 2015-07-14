f72c376a2730dd36af04bdfd3e76a1b64c556c92
Perrine <perrine.ferre@gmail.com>
2015-07-14 18:18:22 -0400

    opt.nucorrect100

--- RANN/rann_fmri_preprocess.m
+++ RANN/rann_fmri_preprocess.m
@@ -178,9 +178,9 @@ opt.smooth_vol.flag_skip = 0;  % Skip spatial smoothing (0: don't skip, 1 : skip
 
 % how to specify a different parameter for two subjects (here subject1 and subject2)
 opt.tune(8).subject = 'P00004507';
-opt.tune(8).param.t1_preprocess.nu_correct.arg = '-distance 50';
+opt.tune(8).param.t1_preprocess.nu_correct.arg = '-distance 100';
 opt.tune(9).subject = 'P00004563';
-opt.tune(9).param.t1_preprocess.nu_correct.arg = '-distance 50';
+opt.tune(9).param.t1_preprocess.nu_correct.arg = '-distance 100';
 
 opt.tune(1).subject = 'P00004216';
 opt.tune(1).param.anat2func.init = 'center';
