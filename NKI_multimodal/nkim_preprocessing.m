84076a05a005e264c833a14f35cfbb69902b4d1c
Pierre Bellec <bellecp@iro.umontreal.ca>
2015-10-16 15:16:58 -0400

    Update nkim_preprocessing.m

--- NKI_multimodal/nkim_preprocessing.m
+++ NKI_multimodal/nkim_preprocessing.m
@@ -181,7 +181,7 @@ opt.smooth_vol.flag_skip = 0;  % Skip spatial smoothing (0: don't skip, 1 : skip
 %% Run the fmri_preprocess pipeline  %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 opt.psom.mode_pipeline_manager = 'background';
-opt.psom.qsub_options = '-q sw -l nodes=1:ppn=1:sandybridge,walltime=48:00:00';
+opt.psom.qsub_options = '-q sw -l nodes=1:ppn=2:pmem=3700m:sandybridge,walltime=48:00:00';
 %opt.granularity = 'subject';
 opt.psom.max_queued = 100;
 opt.time_between_checks = 60;
