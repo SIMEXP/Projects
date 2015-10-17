8f125672b869fec532dc4b74a973f619d0179796
yassinebha <yanamarji@gmail.com>
2015-10-17 12:02:36 -0400

    bug fix missing filesep in the path

--- HCP/hcp_pipeline_scores.m
+++ HCP/hcp_pipeline_scores.m
@@ -2,18 +2,18 @@
 clear all
 exp = 'hcp_preproc';
 task = 'MOTOR';
-path_root = ['/home/yassinebha/scratch/HCP/'];
+path_root = ['/home/yassinebha/database/HCP/'];
 path_preproc = [path_root 'fmri_preprocess_' task '_hcp' ];
 
 %% Get the cambridge templates
-template.path = [path_preproc 'anat/template_cambridge_basc_multiscale_mnc_sym' ];
+template.path = [path_preproc '/anat/template_cambridge_basc_multiscale_mnc_sym' ];
 template.type =  'cambridge_template_mnc';
 niak_wget(template);
 
 %% Select a specific scale for template
 scale = 7 ; 
 template_name = sprintf('template_cambridge_basc_multiscale_sym_scale%03d.mnc.gz',scale);
-system([' cp -r ' template.path filesep template_name ' ' path_preproc 'anat/']);
+system([' cp -r ' template.path filesep template_name ' ' path_preproc '/anat/']);
 
 %% Grab the results from the NIAK fMRI preprocessing pipeline
 opt_g.min_nb_vol = 0;     % The minimum number of volumes for an fMRI dataset to be included. This option is useful when scrubbing is used, and the resulting time series may be too short.
@@ -24,7 +24,7 @@ opt_g.type_files = 'scores'; % Specify to the grabber to prepare the files for t
 files_in = niak_grab_fmri_preprocess(path_preproc,opt_g);
 
 %% Set pipeline options
-opt.folder_out = [path_root 'stability_scores_' task '_' exp '/']; % Where to store the results
+opt.folder_out = [ '/home/yassinebha/scratch/HCP/stability_scores_' task '_' exp '/']; % Where to store the results
 opt.flag_vol = true;
 
 %% Generate the pipeline
