41256357d281bd201d124710abb9e6ae082eaeae
perrineferre <perrine.ferre@gmail.com>
2015-10-27 16:51:21 -0400

    Update rann_fmri_preprocess.m

--- RANN/rann_fmri_preprocess.m
+++ RANN/rann_fmri_preprocess.m
@@ -40,7 +40,6 @@ clear all
 %addpath(genpath('/sb/project/gsf-624-aa/quarantaine/niak-boss-0.13.0'))
 addpath(genpath('/gs/project/gsf-624-aa/quarantaine/niak-boss-0.13.4b'))
 
-<<<<<<< HEAD
 root_path = '/gs/project/gsf-624-aa/RANN/';
 path_out = '/gs/scratch/perrine/RANN/preprocess_data_oct_2015_2/';
 
