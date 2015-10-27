0ac42dd1d8d3603ac847baaf54ee447ab18d518d
perrineferre <perrine.ferre@gmail.com>
2015-10-27 13:14:47 -0400

    niak0.13.4b

--- RANN/rann_fmri_preprocess.m
+++ RANN/rann_fmri_preprocess.m
@@ -38,7 +38,7 @@ clear all
 
 %old-path-removed(unsatisfyingregistratio/slack-general-28092015):
 %addpath(genpath('/sb/project/gsf-624-aa/quarantaine/niak-boss-0.13.0'))
-addpath(genpath('/gs/project/gsf-624-aa/quarantaine/niak-boss-0.13.4'))
+addpath(genpath('/gs/project/gsf-624-aa/quarantaine/niak-boss-0.13.4b'))
 
 root_path = '/gs/project/gsf-624-aa/database2/Projects/RANN/';
 path_out = '/gs/scratch/perrine/RANN/preprocess_data_oct_2015/';
