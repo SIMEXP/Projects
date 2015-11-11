2f818da323183a272ff4c2be5455945c6a370150
AmanPreet Badhwar <amanpreet.badhwar@mail.mcgill.ca>
2015-11-10 22:02:49 -0500

    test, attempting nu_correct on s0146865

--- NKI_multimodal/nkim_preprocessing.m
+++ NKI_multimodal/nkim_preprocessing.m
@@ -59,13 +59,12 @@ exp   = 'all';
 %path_out = '/gs/project/gsf-624-aa/abadhwar/NKI_release3_preprocessed_13_4b/';
 %root_path = '/gs/project/gsf-624-aa/nki_multimodal_release4/';
 %path_out = '/gs/project/gsf-624-aa/abadhwar/NKI_release4_preprocessed_13_4b/';
-%root_path = '/gs/project/gsf-624-aa/nki_multimodal_release1/';
-%path_out = '/gs/project/gsf-624-aa/abadhwar/NKI_146865_preprocessed_13_4b/';
+root_path = '/gs/project/gsf-624-aa/nki_multimodal_release1/';
+path_out = '/gs/project/gsf-624-aa/abadhwar/NKI_146865_preprocessed_13_4b/';
 %root_path = '/gs/project/gsf-624-aa/nki_multimodal_release5/';
 %path_out = '/gs/project/gsf-624-aa/abadhwar/NKI_release5_preprocessed_13_4b/';
-
-root_path = '/gs/project/gsf-624-aa/nki_multimodal_release1/';
-path_out = '/gs/project/gsf-624-aa/abadhwar/NKI_146865_preprocessed_13_4b_rsonly/';
+%root_path = '/gs/project/gsf-624-aa/nki_multimodal_release1/';
+%path_out = '/gs/project/gsf-624-aa/abadhwar/NKI_146865_preprocessed_13_4b_rsonly/';
 
 
 %% Grab the raw data
@@ -91,9 +90,9 @@ for num_s = 1:length(list_subject)
     subject = list_subject{num_s};
     id = ['s' subject];
     files_in.(id).anat = [path_raw subject filesep 'anat' filesep 'mprage.mnc.gz'];
-    %files_in.(id).fmri.sess1.breathHold1400 = [path_raw subject filesep 'TfMRI_breathHold_1400' filesep 'func.mnc.gz'];
-    %files_in.(id).fmri.sess1.checBoard1400 = [path_raw subject filesep 'TfMRI_visualCheckerboard_1400' filesep 'func.mnc.gz'];
-    %files_in.(id).fmri.sess1.checBoard645 = [path_raw subject filesep 'TfMRI_visualCheckerboard_645' filesep 'func.mnc.gz'];    
+    files_in.(id).fmri.sess1.breathHold1400 = [path_raw subject filesep 'TfMRI_breathHold_1400' filesep 'func.mnc.gz'];
+    files_in.(id).fmri.sess1.checBoard1400 = [path_raw subject filesep 'TfMRI_visualCheckerboard_1400' filesep 'func.mnc.gz'];
+    files_in.(id).fmri.sess1.checBoard645 = [path_raw subject filesep 'TfMRI_visualCheckerboard_645' filesep 'func.mnc.gz'];    
     
     files_in.(id).fmri.sess1.rest645 = [path_raw subject filesep 'session_1', filesep,'RfMRI_mx_645' filesep 'rest.mnc.gz'];
     files_in.(id).fmri.sess1.rest1400 = [path_raw subject filesep 'session_1', filesep,'RfMRI_mx_1400' filesep 'rest.mnc.gz'];
@@ -201,6 +200,9 @@ opt.target_space = 'stereolin';
 %opt.tune(3).subject = 's0103714';
 %opt.tune(3).param.slice_timing.arg_nu_correct = '-distance 100';
 %opt.tune(3).param.slice_timing.flag_center = false;
+opt.tune(1).subject = 's0146865';
+opt.tune(1).param.slice_timing.arg_nu_correct = '-distance 100';
+opt.tune(1).param.slice_timing.flag_center = false;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Run the fmri_preprocess pipeline  %%
