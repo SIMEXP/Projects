4b2149e52a7a80d4b502e0d478180a196846dcfa
yassinebha <yanamarji@gmail.com>
2015-11-14 11:41:06 -0500

    WinP

--- HCP/hcp_convert_raw_data.m
+++ HCP/hcp_convert_raw_data.m
@@ -49,10 +49,10 @@ end
 
 list_task = {'EMOTION', 'GAMBLING','LANGUAGE','MOTOR','RELATIONAL','SOCIAL','WM','REST1','REST2'}:
 list_run = {'LR' , 'RL'};
-path_mnc = path_raw ;
-path_nii = [ root_path  'HCP_raw_mnc/' ];
-opt_pipe.path_logs = [path_nii 'logs_conversion'];
-list_subject = dir(path_mnc);
+path_nii = path_raw ;
+path_mnc = [ root_path  'HCP_raw_mnc/' ];
+opt_pipe.path_logs = [path_mnc 'logs_conversion'];
+list_subject = dir(path_nii);
 list_subject = {list_subject(3:end).name};
 nb_subject = length(list_subject);
 %nb_subject = 2;
@@ -60,26 +60,33 @@ pipeline = struct();
 for num_s = 1:nb_subject
     subject = list_subject{num_s};
     fprintf('Subject %s\n',subject);
-    path_sub = [path_mnc subject filesep];
-    path_read.func = [path_sub 'unprocessed/3T'];
-    path_read.anat = [path_sub 'unprocessed/3T/T1w_MPR1'];
-    path_write.anat = [path_nii subject filesep 'MPR_1' filesep];
-    for num_r = 1:length(list_ run)
+    path_sub = [path_nii subject filesep];
+    path_read.func = [path_sub 'unprocessed/3T/'];
+    path_read.anat = [path_sub 'unprocessed/3T/T1w_MPR1/'];
+    path_write.anat = [path_mnc subject filesep 'MPR_1' filesep];
+    for num_r = 1:length(list_run)
           run = list_run{num_r};
           for num_t = 1:length(list_task)
               task = list_task{num_t};
               fprintf('    Task  %s_%s\n', task , run);
-              path_write.func = [path_nii subject filesep task filesep];
-              path_tmp = [path_nii subject filesep task filesep 'tmp' filesep];
+              path_write.func = [path_mnc subject filesep task filesep];
+              path_tmp = [path_mnc subject filesep task filesep 'tmp' filesep];
               name_job = subject ;
               name_job = sprintf('%s_task%s_%s',name_job,num_t,num_r);
-              pipeline.(name_job).files_in       = path_read;
               pipeline.(name_job).files_out.tmp  = path_tmp;
-              pipeline.(name_job).files_out.func = [path_write 'func_' subject '_' task '.nii.gz'];
-              pipeline.(name_job).files_out.anat = [path_write 'anat_' subject '_' task '.nii.gz'];
-              pipeline.(name_job).opt.instr_conv = ['mcverter -o ' path_tmp ' -f nifti -n -u fnformat=-PatientId+PatientName+SequenceName ' path_read];
-              pipeline.(name_job).command        = 'system(opt.instr_conv); twi_mosaic2vol(files_out.tmp,files_out.func,files_out.anat);';        
-              pipeline = psom_add_clean(pipeline,['clean_' name_job],path_tmp);
+              if num_t == 1 && num_r == 1
+                files_out.anat = [path_write.anat 'anat_' subject '_' task 'mnc'];
+                files_out.func = [path_write.func 'func_' subject '_' task '_' lower(run) '.mnc'];
+                pipeline.(name_job).instr_conv = ['nii2mnc ' path_tmp ' -f nifti -n -u fnformat=-PatientId+PatientName+SequenceName ' path_read];
+                pipeline.(name_job).command        = 'system(opt.instr_conv); twi_mosaic2vol(files_out.tmp,files_out.func,files_out.anat);';        
+                pipeline = psom_add_clean(pipeline,['clean_' name_job],path_tmp);
+              else
+                pipeline.(name_job).files_out.anat = [path_write.anat 'anat_' subject '_' task 'mnc'];   
+                pipeline.(name_job).files_out.func = [path_write.func 'func_' subject '_' task '_' lower(run) '.mnc'];
+                pipeline.(name_job).opt.instr_conv = ['mcverter -o ' path_tmp ' -f nifti -n -u fnformat=-PatientId+PatientName+SequenceName ' path_read];
+                pipeline.(name_job).command        = 'system(opt.instr_conv); twi_mosaic2vol(files_out.tmp,files_out.func,files_out.anat);';        
+                pipeline = psom_add_clean(pipeline,['clean_' name_job],path_tmp);
+              end
           end
     end 
 end
