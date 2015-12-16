36217b91dc7fe4bc63bb642391c281a54e1ef1f2
yassinebha <yanamarji@gmail.com>
2015-12-16 17:32:55 -0500

    WinP

---
+++ HCP/hcp_number_of_subjects_per_task.m
@@ -0,0 +1,6 @@
+path_fmri = '/gs/project/gsf-624-aa/HCP/fmri_preprocess_all_tasks_niak/fmri';
+sessions = {'sess1_wmRL ','sess1_wmRL','sess1_gambRL','sess1.wmRL','sess1.gambLR','sess1.motRL','sess1.motLR','sess2.langRL,'sess2.langLR'};
+runs
+for  ss =  
+
+files_fmri = dir([path_fmri  '/
