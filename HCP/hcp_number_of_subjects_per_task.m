dc4ea113c7a0651fe2045f7eb71a09ed37cf8954
yassinebha <yanamarji@gmail.com>
2015-12-18 15:08:26 -0500

    numb subj per task

--- HCP/hcp_number_of_subjects_per_task.m
+++ HCP/hcp_number_of_subjects_per_task.m
@@ -1,6 +1,14 @@
-path_fmri = '/gs/project/gsf-624-aa/HCP/fmri_preprocess_all_tasks_niak/fmri';
-sessions = {'sess1_wmRL ','sess1_wmRL','sess1_gambRL','sess1.wmRL','sess1.gambLR','sess1.motRL','sess1.motLR','sess2.langRL,'sess2.langLR'};
-runs
-for  ss =  
+%count how many subjects for each run on hcp preprocessed tasks
 
-files_fmri = dir([path_fmri  '/
+path_fmri = '/gs/project/gsf-624-aa/HCP/fmri_preprocess_all_tasks_niak/fmri';
+sessions = {'sess1_wmRL','sess1_wmLR','sess1_gambRL',...
+'sess1_gambLR','sess1_motRL','sess1_motLR','sess2_langRL',...
+'sess2_langLR','sess2_socRL','sess2_socLR','sess2_relRL',...
+'sess2_relLR','sess2_emRL','sess2_emLR'};
+num_subj = struct();
+for ss = 1:length(sessions)
+    files_fmri = dir([path_fmri  '/fmri_*_' sessions{ss} '.mnc.gz']);
+    num_subj.(sessions{ss}) = length({files_fmri.name});
+    niak_progress(ss,length(sessions))
+end
+num_subj
