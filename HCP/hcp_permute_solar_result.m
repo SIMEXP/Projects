bfe5b28704ab80b210f009f3ebc5cb1945ed08b9
yassinebha <yanamarji@gmail.com>
2016-01-27 16:57:12 -0500

    WinP

--- HCP/hcp_permute_solar_result.m
+++ HCP/hcp_permute_solar_result.m
@@ -1,10 +1,22 @@
+clear all
 %set path
+path_root = '/home/yassinebha/Google_Drive/HCP/Solar_heritability/twins_yassine_test/';
 
 %load pdigree and pheno
+pedig = niak_read_csv_cell ([path_root 'pedigree_clean.csv']);
+pheno = niak_read_csv_cell([path_root 'phenotypes.csv' ]);
 
 %Generate ID permutaion table (1000 permatation)
+IDs = pheno(2:end,1);
+perm_IDs = {};
+for pp = 1:1000
+      rand('state',pp);
+      order = randperm(length(IDs));
+      perm_IDs_tmp = IDs(order',:);
+      perm_IDs = [perm_IDs perm_IDs_tmp];
+end 
 
-    %loop over permated ID
+    %loop over permuted ID
           %select random non significant pheno from (sub2_net3)
           %build pheno_tmp(i)
           %build pedig_tmp(i)
