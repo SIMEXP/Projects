701e7ce99c2c7f9c97dae1f727091ad9d223a95c
yassinebha <yanamarji@gmail.com>
2016-01-28 18:23:28 -0500

    test completed

--- HCP/hcp_permute_solar_result.m
+++ HCP/hcp_permute_solar_result.m
@@ -1,20 +1,24 @@
 clear all
 %set path
-path_root = '/home/yassinebha/Google_Drive/HCP/Solar_heritability/twins_yassine_test/';
-
+path_root = '/home/yassinebha/Google_Drive/HCP/Solar_heritability/twins_permute_exp/';
+cd(path_root);
 %load pdigree and pheno
-pedig = niak_read_csv_cell ([path_root 'pedigree_clean.csv']);
+system(sprintf('solar <<INTERNAL_SOLAR_SCRIPT \nload pedi %spedigree_clean.csv \nINTERNAL_SOLAR_SCRIPT',path_root))
 pheno = niak_read_csv_cell([path_root 'phenotypes.csv' ]);
 
 %Generate ID permutaion table (1000 permatation)
 IDs = pheno(2:end,1);
 perm_IDs = {};
-for pp = 1:1000
-      rand('state',pp);
-      order = randperm(length(IDs));
-      perm_IDs_tmp = IDs(order',:);
-      perm_IDs = [perm_IDs perm_IDs_tmp];
-end 
+for pp = 1:3
+    rand('state',pp);
+    order = randperm(length(IDs));
+    perm_IDs_tmp = IDs(order',:);
+    pheno(2:end,1)= perm_IDs_tmp;
+    niak_write_csv_cell([path_root 'phenotypes.csv'],pheno);
+    system(['bash fcd_solar_h2r.sh trait_file perm_test' num2str(pp)]);
+    system(['for i in perm_test' num2str(pp) '/Set-*; do bash $i/run_all.sh ; done']);
+    system(['cp -r ' path_root 'perm_test' num2str(pp) '/se_out.out ' path_root 'se_out' num2str(pp) '.out']);
+end
 
     %loop over permuted ID
           %select random non significant pheno from (sub2_net3)
--- HCP/hcp_solar_pedig_pheno.m
+++ HCP/hcp_solar_pedig_pheno.m
@@ -1,13 +1,52 @@
 
-
+% A non generic script to prepare pedigree and pheno variable for solar eclipse.
+% This script will be translated to python for next analysis, and will be coded as 
+% generic script for all HCP tasks and rest
+addpath(genpath('~/github_repos/Projects')) 
 subt_weight = niak_read_csv_cell('/home/yassinebha/Google_Drive/HCP/Solar_heritability/concat_weight_scores_scale7_niak_preproc_task-specific_prior_LR.csv');
 scrub = niak_read_csv_cell('/home/yassinebha/Google_Drive/HCP/Solar_heritability/qc_scrubbing_group.csv');
 pheno_unrestrict = niak_read_csv_cell('/home/yassinebha/Google_Drive/HCP/Solar_heritability/unrestricted_yassinebha_1_13_2016_13_53_17.csv');
 pheno_restrict = niak_read_csv_cell('/home/yassinebha/Google_Drive/HCP/Solar_heritability/RESTRICTED_yassinebha_1_6_2015_14_22_6.csv');
 
-% add sex to pheno table
-subt_weight(:,1)=[] % remove index colomn
-pheno_sex = [pheno_unrestrict(:,1) pheno_unrestrict(:,4)];% select only ID and Gender column 
+% Add sex to pheno table
+subt_weight(:,1)=[]; % Remove index colomn
+pheno_sex = [pheno_unrestrict(:,1) pheno_unrestrict(:,4)];% Select only ID and Gender column 
 pheno_sex = strrep(pheno_sex,'M','1'); % Change M for 1
 pheno_sex = strrep(pheno_sex,'F','2'); % Change F for 2
 concat_weight_sex = combine_cell_tab(subt_weight,pheno_sex);
+
+%Add FD and FD scrubbed to concat_weight_sex
+scrub_FD = [scrub(:,1) scrub(:,4) scrub(:,5)]; % Select IDs, FD and FD scrubbed
+scrub_FD_header = scrub_FD(1,:); %grab the header
+index = strfind(scrub_FD(:,1),'motLR'); % find index matching the task name
+index = find(~cellfun(@isempty,index)); % select only matching index
+scrub_FD_clean = scrub_FD(index,:); % keep only matching task name
+for ii = 1:length(scrub_FD_clean)
+    scrub_FD_clean(ii,1)=scrub_FD_clean{ii,1}(4:end-12); %keep only subject name in the ID
+end
+scrub_FD_clean = [scrub_FD_header ; scrub_FD_clean]; %put back the header
+concat_weight_sex_FD = combine_cell_tab(concat_weight_sex,scrub_FD_clean);
+
+%Build and save pheno Table
+phenotype = [concat_weight_sex_FD(:,1) concat_weight_sex_FD(:,5) concat_weight_sex_FD(:,43) concat_weight_sex_FD(:,6) concat_weight_sex_FD(:,45:46) concat_weight_sex_FD(:,7:41)]; %select specific pheno
+phenotype(1,1)= 'ID'; % Change Subject for ID in the header
+%phenotype(cellfun(@(x) any(isnan(x)),phenotype))=[]; %remove NaN
+%niak_write_csv_cell('/home/yassinebha/Google_Drive/HCP/Solar_heritability/test_pheno.csv',concat_weight_sex_FD);
+niak_write_csv_cell('/home/yassinebha/Google_Drive/HCP/Solar_heritability/phenotypes.csv',phenotype);
+
+% Build pedigree table
+pedigree = [concat_weight_sex_FD(:,1) concat_weight_sex_FD(:,4) concat_weight_sex_FD(:,4) concat_weight_sex_FD(:,43) concat_weight_sex_FD(:,2) concat_weight_sex_FD(:,4)];
+pedigree_header = {'ID','fa','mo','sex','mztwin','hhID'};
+pedigree_tab = pedigree(2:end,:);
+for pp = 1:length(pedigree_tab)
+    pedigree_tab(pp,2)=['fa_' pedigree_tab{pp,2}];%add prefix "fa" for father ID
+    pedigree_tab(pp,3)=['mo_' pedigree_tab{pp,3}];%add prefix "mo" for mother ID
+    if strcmp(pedigree_tab{pp,5}, 'MZ') 
+       pedigree_tab(pp,5)=['pair_' pedigree_tab{pp,6}];%add prefix "pair" if MZ twins, and empty if not
+    else
+       pedigree_tab(pp,5)={''};
+    end
+    pedigree_tab(pp,6)=['hh_' pedigree_tab{pp,6}];%add prefix "hh" household ID
+end
+pedegree_clean = [pedigree_header ;  pedigree_tab];
+niak_write_csv_cell('/home/yassinebha/Google_Drive/HCP/Solar_heritability/pedegree.csv',pedegree_clean);
