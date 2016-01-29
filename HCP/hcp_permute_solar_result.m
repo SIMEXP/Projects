db1985dd58b0e4eca66cf99f679472573c84d89d
yassinebha <yanamarji@gmail.com>
2016-01-29 18:01:51 -0500

    solar solar what is wrong with you

--- HCP/hcp_permute_solar_result.m
+++ HCP/hcp_permute_solar_result.m
@@ -3,21 +3,24 @@ clear all
 path_root = '/home/yassinebha/Google_Drive/HCP/Solar_heritability/twins_permute_exp/';
 cd(path_root);
 %load pdigree and pheno
-system(sprintf('solar <<INTERNAL_SOLAR_SCRIPT \nload pedi %spedigree_clean.csv \nINTERNAL_SOLAR_SCRIPT',path_root))
-pheno = niak_read_csv_cell([path_root 'phenotypes.csv' ]);
+%system(sprintf('solar <<INTERNAL_SOLAR_SCRIPT \nload pedi %spedigree_clean.csv \nINTERNAL_SOLAR_SCRIPT',path_root))
+pheno = niak_read_csv_cell([path_root '/../phenotypes.csv' ]);
 
 %Generate ID permutaion table (1000 permatation)
 IDs = pheno(2:end,1);
 perm_IDs = {};
-for pp = 1:3
+for pp = 1:1000
     rand('state',pp);
     order = randperm(length(IDs));
     perm_IDs_tmp = IDs(order',:);
     pheno(2:end,1)= perm_IDs_tmp;
-    niak_write_csv_cell([path_root 'phenotypes.csv'],pheno);
+    pheno_tmp = [ pheno(:,1:6)   pheno(:,32:36)];
+    niak_write_csv_cell([path_root 'phenotypes.csv'],pheno_tmp);
+    system(sprintf('solar <<INTERNAL_SOLAR_SCRIPT \nload pedi %spedigree_clean.csv \nINTERNAL_SOLAR_SCRIPT',path_root))
+    system(sprintf('solar <<INTERNAL_SOLAR_SCRIPT \nload pheno %sphenotypes.csv \nINTERNAL_SOLAR_SCRIPT',path_root));
     system(['bash fcd_solar_h2r.sh trait_file perm_test' num2str(pp)]);
     system(['for i in perm_test' num2str(pp) '/Set-*; do bash $i/run_all.sh ; done']);
-    system(['cp -r ' path_root 'perm_test' num2str(pp) '/se_out.out ' path_root 'se_out' num2str(pp) '.out']);
+    system(['mv ' path_root 'perm_test' num2str(pp) '/se_out.out ' path_root 'se_out' num2str(pp) '.out']);
 end
 
     %loop over permuted ID
