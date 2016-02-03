fc5fa936f5d56f10a1098d255ee671f0afce7daf
angela-tam <angela.tam08@gmail.com>
2016-02-03 16:20:04 -0500

    new temporary path for niak_niak_brick_stability_tseries

--- adsf/adsf_ct_basc_20160202.m
+++ adsf/adsf_ct_basc_20160202.m
@@ -1,6 +1,7 @@
 % a BASC to generate cortical thickness networks
 
 addpath(genpath('/gs/project/gsf-624-aa/quarantaine/niak-boss-0.13.3'))
+addpath(genpath('/gs/scratch/atam/tmp')) % to point to quick fix for niak_niak_brick_stability_tseries
 
 path = '/home/atam/group_database/database2/preventad/thickness_files_bl_vertex_20150831/';
 in.data = [path 'preventad_civet_vertex_bl_20160202.mat']; % .mat file containing vertex-based cortical thickness measures for each subject
