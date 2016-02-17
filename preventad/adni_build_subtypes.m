b889879720b0f91c884a58251c613933c8c8f39d
Pierre Bellec <pierre.bellec@gmail.com>
2016-02-16 20:24:14 -0500

    Added the option to generate subtypes based on a mask of substructures.

--- preventad/adni_build_subtypes.m
+++ preventad/adni_build_subtypes.m
@@ -1,9 +1,12 @@
-function sub = adni_build_subtypes(data,nb_subtype)
+function sub = adni_build_subtypes(data,nb_subtype,mask)
 % Extract subtypes from functional maps
 % Syntax: SUB = ADNI_BUILD_SUBTYPES(DATA,NB_SUBTYPE)
 %
-% DATA (nb of subjects x nb of voxels) each row is a connectivity map for one subject.
+% DATA (array nb of subjects x nb of voxels) each row is a connectivity map for one subject.
 % NB_SUBTYPE (integer) the number of subtypes.
+% MASK (vector  1 x nb of voxels) a binary mask of voxels of interest. Only these voxels will 
+%    be used for subtyping, although maps will be generated full brain. By default all voxels 
+%    will be used. 
 % SUB (structure) with a bunch of stuff. Fields should be self-explanatory. 
 %
 % (C) Pierre Bellec 2016
@@ -12,13 +15,18 @@ function sub = adni_build_subtypes(data,nb_subtype)
 nb_subject = size(data,1);
 nb_voxels = size(data,2);
 
+%% Default mask
+if nargin < 3 
+    mask = true(1,nb_voxels);
+end
+
 % normalize each map to zero mean and unit variance
 %data = niak_normalize_tseries(data')'; 
 data_d = niak_normalize_tseries(data,struct('type','mean'));
 
 % Perfom a cluster analysis to identify subgroups
 % Inter  subject correlation
-sub.R = niak_build_correlation(data_d'); % Compute inter-subject correlation matrix restricted to the mask
+sub.R = niak_build_correlation(data_d(:,mask)'); % Compute inter-subject correlation matrix restricted to the mask
 % hierarchical clustering
 sub.hier = niak_hierarchical_clustering(sub.R);
 % build an ordering on the subjects based on the hierarchy
@@ -38,6 +46,6 @@ for ss = 1:nb_subtype
     X(:,2) = sub.part==ss;
     [tmp0,tmp,tmp2,sub.map_ttest(ss,:),sub.map_pce(ss,:),sub.map_eff(ss,:),sub.map_std_eff(ss,:)] = niak_lse(data,X,[0 1]);
     for ssub = 1:nb_subject
-        sub.weights(ssub,ss) = corr(sub.map(ss,:)-sub.mean,data(ssub,:)-sub.mean);
+        sub.weights(ssub,ss) = corr(sub.map(ss,mask)-sub.mean(mask),data(ssub,mask)-sub.mean(mask));
     end
 end
