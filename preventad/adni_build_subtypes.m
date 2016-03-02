58e7542ec4a0474ec5914342e9f735ee62f023e4
Pierre Bellec <pierre.bellec@gmail.com>
2016-03-02 12:13:24 -0500

    Added documentation and a flag to demean.

--- preventad/adni_build_subtypes.m
+++ preventad/adni_build_subtypes.m
@@ -1,15 +1,42 @@
-function sub = adni_build_subtypes(data,nb_subtype,mask)
+function sub = adni_build_subtypes(data,nb_subtype,mask,flag_demean)
 % Extract subtypes from functional maps
-% Syntax: SUB = ADNI_BUILD_SUBTYPES(DATA,NB_SUBTYPE)
+%
+% Syntax: SUB = ADNI_BUILD_SUBTYPES(DATA,NB_SUBTYPE,MASK)
 %
 % DATA (array nb of subjects x nb of voxels) each row is a connectivity map for one subject.
 % NB_SUBTYPE (integer) the number of subtypes.
 % MASK (vector  1 x nb of voxels) a binary mask of voxels of interest. Only these voxels will 
 %    be used for subtyping, although maps will be generated full brain. By default all voxels 
 %    will be used. 
-% SUB (structure) with a bunch of stuff. Fields should be self-explanatory. 
+% FLAG_DEMEAN (boolean, default true) Apply demeaning using the global average prior to subtyping. 
+% SUB (structure) each field is an output: 
+%     R (matrix) inter-suject correlation. 
+%     ORDER (vector) an ordering of subjects)
+%     HIER (array) a hierarchical clustering on subjects. 
+%     PART (array 1 x #subjects) the partition of subjects into subgroups. 
+%     MAP (array #subtypes x #voxels) each row is the average map of subtype (de-meaned is FLAG_DEMEAN is true).
+%     WEIGHTS (array #subjects x #subtypes) each column is the list of subtype weights for all subject and a particular subtype. 
 %
-% (C) Pierre Bellec 2016
+% (C) Pierre Bellec, UNF, CRIUGM, DIRO, University of Montreal, 2016.
+% MIT license, see details in the code. 
+
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
 
 % reorganize and normalize data
 nb_subject = size(data,1);
@@ -20,13 +47,20 @@ if nargin < 3
     mask = true(1,nb_voxels);
 end
 
+%% Default demeaning
+if nargin < 4 
+    flag_demean = true;
+end
+
 % normalize each map to zero mean and unit variance
 %data = niak_normalize_tseries(data')'; 
-data_d = niak_normalize_tseries(data,struct('type','mean'));
+if flag_demean 
+    data = niak_normalize_tseries(data,struct('type','mean'));
+end
 
 % Perfom a cluster analysis to identify subgroups
 % Inter  subject correlation
-sub.R = niak_build_correlation(data_d(:,mask)'); % Compute inter-subject correlation matrix restricted to the mask
+sub.R = niak_build_correlation(data(:,mask)'); % Compute inter-subject correlation matrix restricted to the mask
 % hierarchical clustering
 sub.hier = niak_hierarchical_clustering(sub.R);
 % build an ordering on the subjects based on the hierarchy
@@ -35,17 +69,11 @@ sub.order = niak_hier2order(sub.hier);
 sub.part = niak_threshold_hierarchy(sub.hier,struct('thresh',nb_subtype));
 
 %% Compute subtypes and associated weights
-sub.mean = mean(data,1);
 sub.map = zeros(nb_subtype,nb_voxels);
-sub.map_d = zeros(nb_subtype,nb_voxels);
-sub.map_ttest = zeros(nb_subtype,nb_voxels);
 sub.weights = zeros(nb_subject,nb_subtype);
-X = ones([nb_subject 1]);
 for ss = 1:nb_subtype
     sub.map(ss,:) = mean(data(sub.part==ss,:),1);
-    X(:,2) = sub.part==ss;
-    [tmp0,tmp,tmp2,sub.map_ttest(ss,:),sub.map_pce(ss,:),sub.map_eff(ss,:),sub.map_std_eff(ss,:)] = niak_lse(data,X,[0 1]);
     for ssub = 1:nb_subject
-        sub.weights(ssub,ss) = corr(sub.map(ss,mask)-sub.mean(mask),data(ssub,mask)-sub.mean(mask));
+        sub.weights(ssub,ss) = corr(sub.map(ss,mask),data(ssub,mask));
     end
 end
