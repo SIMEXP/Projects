40711e77eafe0faf9772a3072a4cd75cafbf60b8
angela-tam <angela.tam08@gmail.com>
2016-03-11 14:18:20 -0500

    minor edit

--- adsf/adsf_xp_20160304.m
+++ adsf/adsf_xp_20160304.m
@@ -56,7 +56,6 @@ save(file_sub,'sub')
 %% glm to test for associations between subtypes and variables of interest 
 % APOE, BDNF, immediate memory, delayed memory
 
-flag_verbose = false;
 file_res = [path_out 'adsf_glm_20160310.mat'];
 
 % structure containing contrast vectors (1 for variable of interest, 0 for covariates of no interest)
@@ -76,13 +75,7 @@ for nn = 1:nb_net
         mask_nan = max(isnan(model(nn).(contrast).x),[],2);
         model(nn).(contrast).x = model(nn).(contrast).x(~mask_nan,:);
         model(nn).(contrast).y = sub(nn).weights(~mask_nan,:);
-        model(nn).(contrast).c = con_vec.(contrast);  
-        
-        if flag_verbose
-            sum(model(nn).(contrast).x(:,2))
-            sum(~model(nn).(contrast).x(:,2))
-        end
-
+        model(nn).(contrast).c = con_vec.(contrast);
         opt_glm.test = 'ttest';
         glm(nn).(contrast) = niak_glm(model(nn).(contrast),opt_glm);
     end
@@ -95,7 +88,7 @@ for nn = 1:nb_net
     for gg = 1:nb_subt
         for cc = 1:length(list_contrast)
             contrast = list_contrast{cc};
-            figure
+            figure % 4+cc in the following lines because the first four variables in the model are the intercept + confounds
             plot(model(nn).(contrast).x(:,4+cc),model(nn).(contrast).y(:,gg),'o','markersize',7,'markeredgecolor', (col(3,:)), 'markerfacecolor', (col(3,:)+[2 2 2])/3,'linewidth', 0.3);
             hold on
             beta = niak_lse(model(nn).(contrast).y(:,gg),[ones(size(model(nn).(contrast).y(:,gg))) model(nn).(contrast).x(:,4+cc)]);
