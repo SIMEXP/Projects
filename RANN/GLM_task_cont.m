2ae691c26a460ce5b6ed411f91a0db9ee13541f7
perrineferre <perrine.ferre@gmail.com>
2016-05-09 11:13:58 -0400

    back on guillimin + match participants
    40 participants excluded to obtain FD match and equal N of participant <50yo>

--- RANN/GLM_task_cont.m
+++ RANN/GLM_task_cont.m
@@ -1,12 +1,13 @@
 %%% 
 % based on demo GLM CONNECTOME SCRIPT and Angela Tam's "mcinet/mcinet_pipeline_MSPC_MSTEPS_2.m"
-%adapted to continuous variable
+% adapted to continuous variable
 % SIMEXP - Pierre Bellec
 
 clear all
-%path_niak = ('/usr/local/niak/niak-v0.13/')
-%addpath(genpath(path_niak))
-path_data = '/home/pferr/RANN/';
+path_niak = ('/gs/project/gsf-624-aa/quarantaine/niak-issue100/')
+addpath(genpath(path_niak))
+path_data = '/home/perrine/scratch/RANN/'; %% GUILLIMIN
+%%% path_data = '/home/pferr/RANN/'; %% MAGMA 
 
 %%%%%%%%%%%%
 %% Grabbing the results from BASC
@@ -26,6 +27,9 @@ opt_g.type_files = 'glm_connectome'; % Specify to the grabber to prepare the fil
 %opt_g.filter.run = {'ant'}
 opt_g.filter.run = {'ant'}
 %opt_g.exclude_subject = {'P00004840'};%for some reason this participant generates fails in syn (voir GLM_cont_syn1)
+opt_g.exclude_subject = {'P00004547','P00004663','P00004694','P00004636','P00004743','P00004654','P00004819','P00004873','P00004239','P00004574','P00004656','P00004816','P00004742','P00004877','P00004731','P00004306','P00004721','P00004895','P00004646','P00004246','P00002012','P00004841','P00004729','P00004803','P00004667','P00004714','P00004595','P00004343','P00004894','P00004526','P00004609','P00004690','P00004792','P00004861','P00004628','P00004523','P00004617','P00004794','P00004209','P00004356'}
+%% participants excluded to obtain an FD match between age groups <50yo> and same N of participants in each age group
+
 files_in.fmri = niak_grab_fmri_preprocess([path_data 'FINAL_preprocess_test_issue100_16.03.03'],opt_g).fmri; % Replace the folder by the path where the results of the fMRI preprocessing pipeline were stored. 
 
 
@@ -39,7 +43,7 @@ files_in.model.group = [path_data 'BEHAV_all_filters_ant_syn.csv'];
 %%%%%%%%%%%%
 %% Options 
 %%%%%%%%%%%%
-opt.folder_out = [path_data 'RANN_GLMconnectome/GLM_cont_ant4']; % Where to store the resultsb
+opt.folder_out = [path_data 'RANN_GLMconnectome/GLM_cont_ant5']; % Where to store the resultsb
 opt.fdr = 0.1; % The maximal false-discovery rate that is tolerated both for individual (single-seed) maps and whole-connectome discoveries, at each particular scale (multiple comparisons across scales are addressed via permutation testing)
 opt.fwe = 0.05; % The overall family-wise error, i.e. the probablity to have the observed number of discoveries, agregated across all scales, under the global null hypothesis of no association.
 opt.nb_samps = 1000; % The number of samples in the permutation test. This number has to be multiplied by OPT.NB_BATCH below to get the effective number of samples
@@ -66,7 +70,7 @@ opt.test.age.group.select(1).values = 1;
 
 %%% EDUCATION ANT
 opt.test.edu.group.contrast.education = 1; % define contrast of interest
-%%%opt.test.edu.group.contrast.age = 0; % regress out confounding variable
+opt.test.edu.group.contrast.age = 0; % regress out confounding variable
 opt.test.edu.group.contrast.genderMF = 0; % regress out confounding variable
 opt.test.edu.group.contrast.FD_ant = 0; % regress out confounding variable
 opt.test.edu.group.select(1).label = 'filter_in_ant'; % select only antonym tasks (filtered on FD and perf criteria)
@@ -75,7 +79,7 @@ opt.test.edu.group.select(1).values = 1;
 %%% PERFORMANCE ANT
 opt.test.perf.group.contrast.ANT_NumCor100 = 1; % define contrast of interest
 opt.test.perf.group.contrast.education = 0; % regress out confounding variable
-%%%opt.test.perf.group.contrast.age = 0; % regress out confounding variable
+opt.test.perf.group.contrast.age = 0; % regress out confounding variable
 opt.test.perf.group.contrast.genderMF = 0; % regress out confounding variable
 opt.test.perf.group.contrast.FD_ant = 0; % regress out confounding variable
 opt.test.perf.group.select(1).label = 'filter_in_ant'; % select only antonym tasks (filtered on FD and perf criteria)
@@ -89,6 +93,6 @@ opt.flag_test = false; % Put this flag to true to just generate the pipeline wit
 opt.psom.max_queued =  200;       % Number of jobs that can run in parallel. In batch mode, this is usually the number of cores.
 opt.time_between_checks = 90;
 opt.psom.nb_resub = 3;  %verbose opt
-%opt.psom.qsub_options = '-A gsf-624-aa -q sw -l nodes=1:ppn=3,pmem=3700m,walltime=36:00:00'; %so that workers stop beeing killed by walltime after 36h
+opt.psom.qsub_options = '-A gsf-624-aa -q sw -l nodes=1:ppn=3,pmem=3700m,walltime=36:00:00'; %so that workers stop beeing killed by walltime after 36h
 
 [pipeline,opt] = niak_pipeline_glm_connectome(files_in,opt);  
