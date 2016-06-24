ade86d1d4550c1da99ebc920877378973fff3970
yassinebha <yanamarji@gmail.com>
2016-06-24 16:23:21 -0400

    converting from dcm to mnc always fun

--- MAVEN/maven_dcm2nii.m
+++ MAVEN/maven_dcm2nii.m
@@ -1,9 +1,9 @@
 %% script to convert maven database from dicom to mnc
 
 clear
-path_dcm = '/media/database6/MAVEN/dicom/';
-path_nii = '/media/database6/MAVEN/raw_nifti/';
-opt_pipe.path_logs = [path_nii 'logs_conversion'];
+path_dcm = '/media/yassinebha/database24/MAVEN_06_2016/raw_dcm/';
+path_mnc = '/media/yassinebha/database24/MAVEN_06_2016/raw_mnc/';
+opt_pipe.path_logs = [path_mnc 'logs_conversion'];
 list_subject = dir(path_dcm);
 list_subject = {list_subject(3:end).name};
 
@@ -14,14 +14,66 @@ for num_s = 1:nb_subject
     subject = list_subject{num_s};
     fprintf('Subject %s\n',subject);
     path_read      = [path_dcm subject filesep];        
-    path_write     = [path_nii subject filesep];
-    path_tmp       = [path_nii subject filesep 'tmp' filesep];
+    path_write     = [path_mnc subject filesep];
+    path_tmp       = [path_mnc subject filesep 'tmp' filesep];
     path_func_run1 = [path_tmp subject 'RSN1ep2*'];
     path_func_run2 = [path_tmp subject 'RSN2ep2*'];
     path_func_run3 = [path_tmp subject 'RSN3ep2*'];
     path_anat      = [path_tmp subject '*MPRAGEt1*'];
     
+    % create folders
+    % tmp
+    niak_mkdir([path_write 'tmp/']);
+    
+    % run1
+    niak_mkdir([path_write  'func/run1/']);
+    
+    % run2
+    niak_mkdir([path_write  'func/run2/']);
+    
+    % run3
+    niak_mkdir([path_write  'func/run3/']);
+    
+    % anat
+    niak_mkdir([path_write  'anat/']);
+    
+    % convert all to nii in the tmp folder
+    instr_dcm2nii = ['dcm2nii -o ' path_tmp ' ' path_read '*'];
+    
+   
+    % copy nii files to the right folder
+    % run1
+    instr_cp_run1 = ['scp -r ' path_func_run1 ' ' path_write 'func/run1/'];
+    
+    % run2
+    instr_cp_run2 = [' scp -r ' path_func_run2 ' ' path_write 'func/run2/'];
+    
+    % run3
+    instr_cp_run3 = ['scp -r ' path_func_run3 ' ' path_write 'func/run3/'];
+    
+    % anat
+    instr_cp_anat = ['scp -r ' path_anat ' ' path_write 'anat/'];
+    
+    % Final instruction dcm2nii
+    instr_final_dcm2nii = [instr_dcm2nii ';' instr_cp_run1 ';' instr_cp_run2 ';' instr_cp_run3 ';' instr_cp_anat];
+    
+    %convert to minc
+    % convert func images to minc
+    instr_conv_mnc_func = [ 'niak_brick_nii2mnc([' path_write '"func/"],[' path_write '"func/"])'];
+    
+    % convert anat images to minc 
+    instr_conv_mnc_anat = [ 'niak_brick_nii2mnc([' path_write '"anat/"],[' path_write '"anat/"])'];
+    
+    % remove func nifti files 
+    instr_rm_func_nii = ['rm -r ' path_write 'func/*/*.nii'];
+    
+    % remove anat nifti file
+    instr_rm_anat_nii = ['rm -r ' path_write 'anat/*.nii']; 
+    
+    %start jobs
     name_job   = sprintf('%s',subject);
+    
+    
     pipeline.(name_job).files_in            = path_read;
     pipeline.(name_job).files_out.tmp       = path_tmp;
     pipeline.(name_job).files_out_tmp       = ['mkdir -p ' path_write 'tmp/']
