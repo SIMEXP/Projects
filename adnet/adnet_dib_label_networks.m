64992badd5479351c38b0fbbd0e722a5afcf0183
angela-tam <angela.tam08@gmail.com>
2016-07-07 20:33:34 -0400

    looping over networks to generate labels

--- adnet/adnet_dib_label_networks.m
+++ adnet/adnet_dib_label_networks.m
@@ -25,21 +25,18 @@ clear all
 path_data = '/Users/AngelaTam/Desktop/data_in_brief/parcellations/';
 path_clus = [path_data 'template_mcinet_basc_sym_clusters_nii/'];
 
-files_in.cluster = [path_data 'scale_6_networks_sym/salience.nii.gz'];
+network = {'cerebellum','dmn','limbic','motor','salience','visual'}; % names of networks in scale 6
+nb_clus = [12 22 33 65 111 208]; % scales
 
-nb_clus = [12 22 33 65 111 208];
-
-for nn = 1:length(nb_clus)
-
-    files_in.subcluster = [path_clus 'brain_parcellation_mcinet_basc_sym_' num2str(nb_clus(nn)) 'clusters.nii.gz'];
-    
-    files_out = struct;
-    files_out.subcluster = {strcat('salience_res', num2str(nb_clus(nn)), '.nii.gz')};
-    opt.folder_out = [path_clus 'labels/'];
-    
-    niak_brick_subclusters(files_in,files_out,opt);
-
+for cc = 1:length(network)
+    files_in.cluster = strcat(path_data, 'scale_6_networks_sym/', network{cc}, '.nii.gz');
+    for nn = 1:length(nb_clus)
+        files_in.subcluster = [path_clus 'brain_parcellation_mcinet_basc_sym_' num2str(nb_clus(nn)) 'clusters.nii.gz'];
+        files_out = struct;
+        files_out.subcluster = {strcat(network{cc}, '_res', num2str(nb_clus(nn)), '.nii.gz')};
+        opt.perc_overlap = 0.5;
+        niak_brick_subclusters(files_in,files_out,opt);
+    end
 end
-
 
 
