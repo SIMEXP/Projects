5bca6ad400f90a450c0579b346a83b8ceeb1361b
Perrine Ferre <perrine@lg-1r17-n02.guillimin.clumeq.ca>
2016-07-15 16:05:55 -0400

    interactions effects added

--- RANN/GLM_task_cont.m
+++ RANN/GLM_task_cont.m
@@ -24,8 +24,8 @@ opt_g.type_files = 'glm_connectome'; % Specify to the grabber to prepare the fil
 % opt_g.filter.session = {'session1'}; % Just grab session 1
 
 %% select one task or another:
-opt_g.filter.run = {'ant'}
 %opt_g.filter.run = {'syn'}
+opt_g.filter.run = {'ant'}
 % exclude subjects synonyms:
 %opt_g.exclude_subject = {'P00004840'}
 %opt_g.exclude_subject = {'P00002012', 'P00004654','P00004663','P00004694','P00004742','P00004743','P00004501','P00004551','P00004571','P00004636','P00004648','P00004719','P00004787','P00004797','P00004805','P00004825','P00004828'}
@@ -46,7 +46,7 @@ files_in.model.group = [path_data 'BEHAV_all_filters_ant_syn.csv'];
 %%%%%%%%%%%%
 %% Options 
 %%%%%%%%%%%%
-opt.folder_out = [path_data 'RANN_GLMconnectome/GLM_cont_ant160625']; % Where to store the resultsb
+opt.folder_out = [path_data 'RANN_GLMconnectome/GLM_cont_ant_inter_test_160715']; % Where to store the resultsb
 opt.fdr = 0.1; % The maximal false-discovery rate that is tolerated both for individual (single-seed) maps and whole-connectome discoveries, at each particular scale (multiple comparisons across scales are addressed via permutation testing)
 opt.fwe = 0.05; % The overall family-wise error, i.e. the probablity to have the observed number of discoveries, agregated across all scales, under the global null hypothesis of no association.
 opt.nb_samps = 1000; % The number of samples in the permutation test. This number has to be multiplied by OPT.NB_BATCH below to get the effective number of samples
@@ -61,32 +61,89 @@ opt.flag_rand = false; % if the flag is false, the pipeline is deterministic. Ot
 %% 1-Group differences: age
 %% 2-Group differences: education 
 %% 3- Group differences: performance
-%% 4- interactions ?????
+%% 4- interactions
 
-%%% AGE ANT
-opt.test.age.group.contrast.age = 1; % define contrast of interest (age continuous only)
-opt.test.age.group.contrast.education = 0; % regress out confounding variable
-opt.test.age.group.contrast.genderMF = 0; % regress out confounding variable
-opt.test.age.group.contrast.FD_ant = 0; % regress out confounding variable
-opt.test.age.group.select(1).label = 'filter_in_ant'; % select only antonym tasks (filtered on FD and perf criteria)
-opt.test.age.group.select(1).values = 1;
+%%% INTER AGExEDU
+%opt.test.interaction_age_edu.group.interaction.label = 'inter_agexedu'
+%opt.test.interaction_age_edu.group.interaction.factor = {'age','education'}
+%opt.test.interaction_age_edu.group.contrast.age = 0; % define contrast of interest (age continuous only)
+%opt.test.interaction_age_edu.group.contrast.education = 0; % regress out confounding variable
+%opt.test.interaction_age_edu.group.contrast.genderMF = 0; % regress out confounding variable
+%opt.test.interaction_age_edu.group.contrast.FD_ant = 0; % regress out confounding variable
+%opt.test.interaction_age_edu.group.contrast.ANT_NumCor100 = 0; % define contrast of interest SYN continuous only)
+%opt.test.interaction_age_edu.group.contrast.inter_agexedu = 1;
+%opt.test.interaction_age_edu.group.select(1).label = 'filter_in_ant'; % select only a task (filtered on FD and perf criteria)
+%opt.test.interaction_age_edu.group.select(1).values = 1;
+
+
+%%% INTER AGExPERF
+%opt.test.interaction_age_ANTperf.group.interaction.label = 'inter_agexANT'
+%opt.test.interaction_age_ANTperf.group.interaction.factor = {'age','ANT_NumCor100'}
+%opt.test.interaction_age_ANTperf.group.contrast.age = 0; % define contrast of interest age continuous only)
+%opt.test.interaction_age_ANTperf.group.contrast.ANT_NumCor100 = 0; % define contrast of interest SYN continuous only)
+%opt.test.interaction_age_ANTperf.group.contrast.education = 0; % regress out confounding variable
+%opt.test.interaction_age_ANTperf.group.contrast.genderMF = 0; % regress out confounding variable
+%opt.test.interaction_age_ANTperf.group.contrast.FD_ant = 0; % regress out confounding variable
+%opt.test.interaction_age_ANTperf.group.contrast.inter_agexANT = 1;
+%opt.test.interaction_age_ANTperf.group.select(1).label = 'filter_in_ant'; % select only a task (filtered on FD and perf criteria)
+%opt.test.interaction_age_ANTperf.group.select(1).values = 1;
+
+%%% INTER PERFxEDU
+opt.test.interaction_ANTperf_edu.group.interaction.label = 'inter_eduxANT'
+opt.test.interaction_ANTperf_edu.group.interaction.factor = {'education','ANT_NumCor100'}
+opt.test.interaction_ANTperf_edu.group.contrast.education = 0; % define contrast of interest age continuous only)
+opt.test.interaction_ANTperf_edu.group.contrast.ANT_NumCor100 = 0; % define contrast of interest SYN continuous only)
+%opt.test.interaction_ANTperf_edu.group.contrast.age = 0; % regress out confounding variable
+opt.test.interaction_ANTperf_edu.group.contrast.genderMF = 0; % regress out confounding variable
+opt.test.interaction_ANTperf_edu.group.contrast.FD_ant = 0; % regress out confounding variable
+opt.test.interaction_ANTperf_edu.group.contrast.inter_eduxANT = 1;
+opt.test.interaction_ANTperf_edu.group.select(1).label = 'filter_in_ant'; % select only a task (filtered on FD and perf criteria)
+opt.test.interaction_ANTperf_edu.group.select(1).values = 1;
+
+
+%%%% MAIN EFFECTS %%%%
+
+%%% FD
+%opt.test.FD.group.contrast.FD_ant = 1; % define contrast of interest (FD continuous only)
+%opt.test.FD.group.contrast.education = 0; % regress out confounding variable
+%opt.test.FD.group.contrast.genderMF = 0; % regress out confounding variable
+%opt.test.FD.group.contrast.age = 0; % regress out confounding variable
+%opt.test.FD.group.select(1).label = 'filter_in_ant'; % select only a task (filtered on FD and perf criteria)
+%opt.test.FD.group.select(1).values = 1;
+
+
+%%% SEXE
+%opt.test.sexe.group.contrast.genderMF = 1; % define contrast of interest (FD continuous only)
+%opt.test.sexe.group.contrast.education = 0; % regress out confounding variable
+%opt.test.sexe.group.contrast.ANT_NumCor100 = 0; % regress out confounding variable
+%opt.test.sexe.group.contrast.age = 0; % regress out confounding variable
+%opt.test.sexe.group.select(1).label = 'filter_in_ant'; % select only a task (filtered on FD and perf criteria)
+%opt.test.sexe.group.select(1).values = 1;
+
+%%% AGE
+%opt.test.age.group.contrast.age = 1; % define contrast of interest (age continuous only)
+%opt.test.age.group.contrast.education = 0; % regress out confounding variable
+%opt.test.age.group.contrast.genderMF = 0; % regress out confounding variable
+%opt.test.age.group.contrast.FD_syn = 0; % regress out confounding variable
+%opt.test.age.group.select(1).label = 'filter_in_syn'; % select only a task (filtered on FD and perf criteria)
+%opt.test.age.group.select(1).values = 1;
 
 %%% EDUCATION ANT
-opt.test.edu.group.contrast.education = 1; % define contrast of interest
-opt.test.edu.group.contrast.age = 0; % regress out confounding variable
-opt.test.edu.group.contrast.genderMF = 0; % regress out confounding variable
-opt.test.edu.group.contrast.FD_ant = 0; % regress out confounding variable
-opt.test.edu.group.select(1).label = 'filter_in_ant'; % select only antonym tasks (filtered on FD and perf criteria)
-opt.test.edu.group.select(1).values = 1;
+%opt.test.edu.group.contrast.education = 1; % define contrast of interest
+%opt.test.edu.group.contrast.age = 0; % regress out confounding variable
+%opt.test.edu.group.contrast.genderMF = 0; % regress out confounding variable
+%opt.test.edu.group.contrast.FD_syn = 0; % regress out confounding variable
+%opt.test.edu.group.select(1).label = 'filter_in_syn'; % select only a task (filtered on FD and perf criteria)
+%opt.test.edu.group.select(1).values = 1;
 
 %%% PERFORMANCE ANT
-opt.test.perf.group.contrast.ANT_NumCor100 = 1; % define contrast of interest
-opt.test.perf.group.contrast.education = 0; % regress out confounding variable
-opt.test.perf.group.contrast.age = 0; % regress out confounding variable
-opt.test.perf.group.contrast.genderMF = 0; % regress out confounding variable
-opt.test.perf.group.contrast.FD_ant = 0; % regress out confounding variable
-opt.test.perf.group.select(1).label = 'filter_in_ant'; % select only antonym tasks (filtered on FD and perf criteria)
-opt.test.perf.group.select(1).values = 1;
+%opt.test.perf.group.contrast.SYN_NumCor100 = 1; % define contrast of interest
+%opt.test.perf.group.contrast.education = 0; % regress out confounding variable
+%opt.test.perf.group.contrast.age = 0; % regress out confounding variable
+%opt.test.perf.group.contrast.genderMF = 0; % regress out confounding variable
+%opt.test.perf.group.contrast.FD_syn = 0; % regress out confounding variable
+%opt.test.perf.group.select(1).label = 'filter_in_syn'; % select only a task (filtered on FD and perf criteria)
+%opt.test.perf.group.select(1).values = 1;
 
 
 %%%%%%%%%%%%
