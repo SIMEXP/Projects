b885358ea3c560d7578c05309c01aa9bb1179c8e
angela-tam <angela.tam08@gmail.com>
2016-08-10 01:39:53 -0400

    added confound regression

--- adsf/adsf_ct_network_stack.m
+++ adsf/adsf_ct_network_stack.m
@@ -21,6 +21,10 @@ function [files_in,files_out,opt] = adsf_ct_network_stack(files_in,files_out,opt
 %            PART (vector) PART(I) = J if the object I is in the class J.
 %               See also: niak_threshold_hierarchy
 %
+%   MODEL
+%       (optional, string) path to a .csv containining values for each
+%       subject for confound variables
+%
 % FILES_OUT
 %   (structure) with the field:
 %   
@@ -37,6 +41,12 @@ function [files_in,files_out,opt] = adsf_ct_network_stack(files_in,files_out,opt
 %
 %   NB_NETWORK
 %       (integer) number of networks (or resolution/scale)
+%
+%   REGRESS_CONF 
+%       (Cell of string, Default {}) A list of variables name to be regressed out.
+%
+%   FLAG_CONF
+%       (boolean, default true) turn on/off for confound regression
 %
 %   FLAG_VERBOSE
 %       (boolean, default true) turn on/off the verbose.
@@ -56,13 +66,13 @@ end
 
 % Input
 files_in = psom_struct_defaults(files_in,...
-           { 'data' , 'partition' },...
-           { NaN    , NaN         });
+           { 'data' , 'partition' , 'model'           },...
+           { NaN    , NaN         , 'gb_niak_omitted' });
 
 % Options
 opt = psom_struct_defaults(opt,...
-      { 'folder_out' , 'nb_network' , 'flag_verbose' , 'flag_test' },...
-      { ''           , NaN          , true           , false       });
+      { 'folder_out' , 'nb_network' , 'regress_conf', 'flag_conf', 'flag_verbose' , 'flag_test' },...
+      { ''           , NaN          , {}            , true       , true           , false       });
 
 % Output
 if ~isempty(opt.folder_out)
@@ -121,22 +131,88 @@ end
 
 %% build the stacks
 
+% save subject list in provenance
+provenance.subjects = data.subjects;
+
 % make an empty 2d array
 ct_net = zeros(n_sub,n_vox); 
 mname = fieldnames(tmp_mask);
 
-% save subject list in provenance
-provenance.subjects = data.subjects;
-
-% fill array with cortical thickness values per netwok per subject
+% loop over networks
+% fill array with cortical thickness values per subject
 for cc = 1:opt.nb_network
-    stack(:,:) = ct.*tmp_mask.(mname{cc});
+    r_stack = ct.*tmp_mask.(mname{cc});
     provenance.network = cc;
-    if ~strcmp(files_out.stack, 'gb_niak_omitted')
-        save(files_out.stack{cc}, 'stack', 'provenance')
+    
+    %% confound regression
+    
+    if ~strcmp(files_in.model, 'gb_niak_omitted')
+        [tab,sid,ly,~] = niak_read_csv(files_in.model);
+        
+        n_conf = length(opt.regress_conf);
+        conf_ids = zeros(n_conf, 1);
+        for cid = 1:n_conf
+            conf_name = opt.regress_conf{cid};
+            cidx = find(strcmp(ly, conf_name));
+            % Make sure we found the covariate
+            if ~isempty(cidx)
+                conf_ids(cid) = cidx;
+            else
+                error('Could not find column for %s in model file');
+            end
+            % Make sure there are no NaNs in the model
+            if any(isnan(tab(:, cidx)))
+                % Get the indices of the subjects
+                missing = find(isnan(tab(:, cidx)));
+                % Matlab error messages only allow for the double to iterate. Not
+                % sure how we could tell them both the subject ID and the confound
+                % name
+                error('Subject #%d has missing data for one or more confounds. Please fix.\n', missing);
+            end
+        end
+        
+        % Match subjects in model with those we have data for
+        s_list = zeros(n_sub, 1);
+        for ss = 1:n_sub
+            ct_id = provenance.subjects{ss};
+            l_sub = find(strcmp(sid, ct_id));
+            if ~isempty(l_sub)
+                s_list(ss) = l_sub;
+            end
+        end
+        
+        % Set up the model structure for the regression
+        opt_mod = struct;
+        opt_mod.flag_residuals = true;
+        m = struct;
+        m.x = zeros(length(s_list), n_conf+1);
+        for ss = 1:length(s_list)
+            m.x(ss,:) = [1 tab(s_list(ss), conf_ids)];
+        end
+        c_stack = zeros(n_sub, n_vox);
+        
+        % Do the regression
+        m.y = r_stack;
+        [res] = niak_glm(m, opt_mod);
+        % Store the residuals in the confound stack
+        c_stack = res.e;
     end
+    
+    %% Build the outputs
+    % Decide which of the two stacks to save
+    if opt.flag_conf && ~strcmp(files_in.model, 'gb_niak_omitted')
+        stack = c_stack;
+        provenance.model = struct;
+        provenance.model.matrix = m.x;
+        provenance.model.confounds = opt.regress_conf;
+    else
+        stack = r_stack;
+    end
+    
+    % Save the stack matrix
+    save(files_out.stack{cc}, 'stack', 'provenance');
+    
 end
-
 end
 
 function path_array = make_paths(out_path, template, scales)
