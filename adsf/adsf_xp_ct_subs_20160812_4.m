aa0fff1d24c2722c86cc236f91800c37f6c83b5d
angela-tam <angela.tam08@gmail.com>
2016-08-13 18:11:24 -0400

    xp for 4 subtypes

--- adsf/adsf_xp_ct_subs_20160812_4.m
+++ adsf/adsf_xp_ct_subs_20160812_4.m
@@ -3,19 +3,20 @@
 %% make the subtypes
 clear all
 
-path_data = '/home/atam/scratch/adsf/adsf_ct_stack/';
-path_out = '/home/atam/scratch/adsf/ct_subtypes_20160812_4/';
+path_data = '/Users/AngelaTam/Desktop/adsf/ct_stack_age_gender/';
+path_out = '/Users/AngelaTam/Desktop/adsf/ct_subtypes_20160812_4sub/';
 psom_mkdir(path_out);
 
 for ss = 1:9
     files_in.data = strcat(path_data, 'ct_network_', num2str(ss), '_stack.mat');
     files_in.mask = strcat(path_data, 'mask_network', num2str(ss), '.mat');
     files_out = struct;
-    opt.nb_subtype = 5;
+    opt.nb_subtype = 4;
     opt.folder_out = [path_out, 'net', num2str(ss)];
     psom_mkdir(opt.folder_out)
     
     adsf_brick_subtyping(files_in,files_out,opt);
+    clear files_in
 end
 
 %% make the maps
@@ -31,13 +32,14 @@ for ss = 1:9
     opt.nb_subtype = 4;
     
     adsf_brick_visu_ct_sub(files_in,files_out,opt);
+    clear files_in
 end
 
 %% weight extraction
 
 clear all
-path_stack = '/Users/AngelaTam/Desktop/adsf/ct_stack/';
-path_sub = '/home/atam/scratch/adsf/ct_subtypes_20160812_4/';
+path_stack = '/Users/AngelaTam/Desktop/adsf/ct_stack_age_gender/';
+path_sub = '/Users/AngelaTam/Desktop/adsf/ct_subtypes_20160812_4sub/';
 
 for ss = 1:9
     files_in.data.net = strcat(path_stack, 'ct_network_', num2str(ss), '_stack.mat');
@@ -46,4 +48,5 @@ for ss = 1:9
     opt.folder_out = strcat(path_sub, 'net', num2str(ss));
     
     adsf_brick_subtype_weight(files_in,files_out,opt);
+    clear files_in
 end
