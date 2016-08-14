09d4da25b0b3090640c8ed0e52ccaf73e509f0c0
angela-tam <angela.tam08@gmail.com>
2016-08-13 22:58:34 -0400

    calculating mean ct per network

--- adsf/adsf_mean_ct.m
+++ adsf/adsf_mean_ct.m
@@ -17,3 +17,35 @@ for ss = 1:222
     labels{ss+1,2} = mean(ct(ss,:));
     fprintf(fid, '%s, %d\n', labels{ss+1,:});
 end
+
+%% calculate mean thickness for one network per subject
+
+clear all
+
+for net = 1:9  % for all 9 networks
+    % load the files
+    ct_file = strcat('ct_network_', num2str(net), '_stack.mat');
+    mask_file = strcat('mask_network', num2str(net), '.mat');
+    load(mask_file); % loading mask for network
+    n_vox = sum(mask); % number of vertices within network mask
+    load(ct_file); % loading raw stack file
+    
+    % set up the csv
+    labels = cell(223,2);
+    labels{1,1} = 'subject';
+    labels{1,2} = strcat('mean_ct_net',num2str(net));
+    csvname = strcat('raw_whole_brain_mean_ct_net', num2str(net), '.csv');
+    fid = fopen(csvname,'w');
+    fprintf(fid, '%s, %s\n', labels{1,:});
+    
+    % fill in the cells
+    for ss = 1:222 % for all 222 subjects
+        labels{ss+1,1} = provenance.subjects{ss};
+        labels{ss+1,2} = sum(stack(ss,:))/n_vox;
+        fprintf(fid, '%s, %d\n', labels{ss+1,:});
+    end
+    fclose(fid)
+end
+    
+    
+    
