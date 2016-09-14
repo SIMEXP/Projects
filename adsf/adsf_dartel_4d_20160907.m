2da6b16a7bcc902188fea2d1f798fe3135ef010d
angela-tam <angela.tam08@gmail.com>
2016-09-14 00:49:28 -0400

    update to grab more subjects

--- adsf/adsf_dartel_4d_20160907.m
+++ adsf/adsf_dartel_4d_20160907.m
@@ -3,8 +3,8 @@
 clear all
 
 path_in = '/home/atam/scratch/adni_dartel/raw_mnc/';
-path_out = '/home/atam/scratch/adni_dartel/stack_mnc/';
-path_model = '/home/atam/scratch/adni_dartel/adni_model_20160906.csv';
+path_out = '/home/atam/scratch/adni_dartel/stack_mnc_20160912/';
+path_model = '/home/atam/scratch/adni_dartel/model/adni_dartel_model_20160912.csv';
 ext_v = '.mnc.gz';
 
 stack = adsf_brick_3d_to_4d(path_in,path_out,path_model,ext_v);
