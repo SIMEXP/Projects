2122bda74547e469e384eee616e6f72bbe7b8759
angela-tam <angela.tam08@gmail.com>
2016-09-16 15:03:16 -0400

    update to script

--- adsf/adsf_dartel_subtype_wb_preventad_20160915.m
+++ adsf/adsf_dartel_subtype_wb_preventad_20160915.m
@@ -4,11 +4,11 @@ clear all
 
 %% set up paths
 path_data = '/gs/project/gsf-624-aa/database2/preventad/dartel_villeneuve_20160817_mnc/';
-path_out = '/home/atam/scratch/dartel_subtypes/preventad_dartel/wb_subtypes_adniext_20160915/';
+path_out = '/home/atam/scratch/dartel_subtypes/preventad_dartel/wb_subtypes_adniext_20160916/';
 
 %% set up files_in structure
 
-files_in.model = '/home/atam/scratch/dartel_subtypes/preventad_dartel/model/preventad_model_condensed_20160915.csv';
+files_in.model = '/home/atam/scratch/dartel_subtypes/preventad_dartel/model/preventad_model_20160916.csv';
 files_in.mask = '/home/atam/scratch/dartel_subtypes/adni_dartel/mask/mask_gm_dartel_adni.mnc.gz';
 files_in.subtype.network_1 = '/home/atam/scratch/dartel_subtypes/adni_dartel/dartel_wb_subtypes_20160912/network_1/network_1_subtype.mat';
 
@@ -46,11 +46,13 @@ end
 
 opt.folder_out = path_out;
 opt.scale = 1;
-opt.stack.regress_conf = {'age','gender'};
+opt.stack.regress_conf = {'age','gender','tiv','mean_gm_wb'};
 opt.subtype.nb_subtype = 4;
-opt.flag_assoc = false;
+opt.flag_assoc = true;
+opt.association.contrast.delayed_memory_index_score = 1;
 opt.flag_chi2 = false;
-opt.flag_visu = false;
+opt.flag_visu = true;
+opt.visu.data_type = 'continuous';
 
 %% run pipeline
 [pipe,opt] = niak_pipeline_subtype(files_in,opt);
