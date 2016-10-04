ce869773357062d08d41d26649d23c73dffb986f
angela-tam <angela.tam08@gmail.com>
2016-10-04 16:19:41 -0400

    glm dartel subtypes moca

---
+++ adsf/adsf_xp_dartel_glm_moca_adni_20161003.m
@@ -0,0 +1,27 @@
+%% glm on whole brain dartel adni subtypes on moca
+
+clear all
+
+a_path = '/Users/AngelaTam/Desktop/adsf/dartel_subtypes/adni_dartel/dartel_wb_subtypes_20160926/';
+files_in.weight = [a_path 'subtype_weights.mat'];
+files_in.model = '/Users/AngelaTam/Desktop/adsf/dartel_subtypes/adni_dartel/adni_dartel_model_20160930.csv';
+files_out = struct;
+opt.folder_out = [a_path 'glm_moca_codxedu_20161004'];
+opt.scale = 1;
+opt.contrast.MOCA = 1;
+opt.contrast.age = 0;
+opt.contrast.sex = 0;
+opt.contrast.tiv = 0;
+opt.contrast.mean_gm = 0;
+opt.contrast.diagnosis = 0;
+opt.contrast.education = 0;
+
+niak_brick_association_test(files_in,files_out,opt);
+
+% glm visu
+
+files_in.association = [a_path 'glm_moca_codxedu_20161004/association_stats.mat'];
+files_out = struct;
+opt.data_type = 'continuous';
+
+niak_brick_visu_subtype_glm(files_in,files_out,opt);
