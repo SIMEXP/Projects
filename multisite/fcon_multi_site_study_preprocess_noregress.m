daaa8d02693a82703df8898fbf17e6c92e31542f
Christian Dansereau <cdansereau@lg-1r17-n03.guillimin.clumeq.ca>
2016-10-09 11:13:03 -0400

    script no regress psom2

--- multisite/fcon_multi_site_study_preprocess_noregress.m
+++ multisite/fcon_multi_site_study_preprocess_noregress.m
@@ -3,26 +3,26 @@
 % specified dataset (1000 fcon dataset)
 clear all
 
-p=genpath('/home/bellec_group/niak-boss-0.12.2/');
+p=genpath('/home/cdansereau/database/quarantaine/niak-v0.17.0/');
 addpath(p);
 
 
 pipeline = [];
 opt_pipe = [];
-sites = {'Atlanta', 'Baltimore', 'Berlin', 'Cambridge', 'Newark', 'NewYork_b', 'Oxford', 'Queensland', 'SaintLouis' };
+sites = {'Baltimore', 'Berlin', 'Cambridge', 'Newark', 'NewYork_b', 'Oxford', 'Queensland', 'SaintLouis' };
 
 for idx_sites = 1:length(sites)
-	path_raw_fmri   = ['/home/danserea/database/multisite/fcon_1000_raw/raw_mnc/' sites{idx_sites} '/'];
+	path_raw_fmri   = ['/home/cdansereau/database/multisite/raw_mnc_1kfcon/' sites{idx_sites} '/'];
         %path_preprocess = '/sb/project/gsf-624-aa/database/aveugle/fmri_preprocess_exp3/';
-        path_preprocess = ['/home/danserea/database/scrubbing/fcon_1000_preprocess/' sites{idx_sites} '/fmri_preprocess_05scrubb/'];
+        path_preprocess = ['/home/cdansereau/database/multisite/fcon_1000_preprocess_noreg/' sites{idx_sites} '/'];
 	%/data/lepore/mpelland/minc_conv/anat/CB/VD_AlCh
 
 	files_in = fcon_grab_raw(path_raw_fmri);
 
 	clear opt;
 	%motion+
-	%opt.regress_confounds.flag_wm = true;
-	%opt.regress_confounds.flag_vent = true;
+	opt.regress_confounds.flag_wm = false;
+	opt.regress_confounds.flag_vent = false;
 	%opt.regress_confounds.flag_motion_params = true;
 	opt.regress_confounds.flag_scrubbing = true;
 	opt.regress_confounds.thre_fd = 0.5;
@@ -42,8 +42,8 @@ for idx_sites = 1:length(sites)
 
 	%% Slice timing
 	opt.slice_timing.flag_skip = true;
-	%opt.slice_timing.type_acquisition = 'interleaved ascending'; % Slice timing order (available options : 'sequential ascending', 'sequential descending', 'interleaved ascending', 'interleaved descending')
-	%opt.slice_timing.type_scanner     = 'Siemens';               % Scanner manufacturer. Only the value 'Siemens' will actually have an impact
+	opt.slice_timing.type_acquisition = 'interleaved ascending'; % Slice timing order (available options : 'sequential ascending', 'sequential descending', 'interleaved ascending', 'interleaved descending')
+	opt.slice_timing.type_scanner     = 'Siemens';               % Scanner manufacturer. Only the value 'Siemens' will actually have an impact
 	%opt.slice_timing.delay_in_tr      = 0;                       % The delay in TR ("blank" time between two volumes)
 
 	%% Motion correction (niak_brick_motion_correction)
@@ -72,7 +72,7 @@ for idx_sites = 1:length(sites)
 	opt.bricks.smooth_vol.fwhm = 6; % Apply an isotropic 6 mm gaussin smoothing.
 
 	%% Region growing
-	opt.region_growing.flag_skip = true; % Turn on/off the region growing
+	opt.region_growing.flag_skip = false; % Turn on/off the region growing
 	%opt.template_fmri = '/home/cdansereau/svn/niak/trunk/template/roi_aal.mnc.gz';
 
 	%% PVE
@@ -110,7 +110,8 @@ for idx_sites = 1:length(sites)
 	end
 
 %opt_tmp.psom.restart = {'sub88445','sub78552'};
-opt_tmp.psom.qsub_options = '-q qwork@ms -l walltime=10:00:00';
+opt_tmp.psom.qsub_options = '-A gsf-624-aa -l walltime=07:00:00';
+opt_tmp.psom.path_logs='/home/cdansereau/database/multisite/fcon_1000_preprocess_noreg/logs'
 psom_run_pipeline(pipeline,opt_tmp.psom)
 
 
