61cc626a957c9247746ff96127dd7ab355679800
Pierre Bellec <bellecp@iro.umontreal.ca>
2016-10-28 13:54:05 -0400

    Update cobre_upload_figshare.sh

--- cobre/cobre_upload_figshare.sh
+++ cobre/cobre_upload_figshare.sh
@@ -5,11 +5,24 @@ curl -i -H "Content-Type: application/json" -X POST --data '{"authors": [{"name"
 
 # Before running this next command, create a variable $id with the article id returned by the previous command. It can also be set to the id of an existing article, generated previously or generated with the gui
 
-# Upload files
-for file in "$search_dir"*
+# Create file list
+list_file=("$search_dir"*)
+for file in ${list_file[@]}
 do
   md5_file="$(md5sum $file)"
   size_file=$(stat -c%s "$file")
   arg='{"md5": "'$md5_file'", "name": "'$file'", "size": '$size_file'}'
   curl -i -H "Content-Type: application/json" -X POST --data "$arg" https://api.figshare.com/v2/account/articles/$id/files?access_token=$token  
 done
+
+# Retrieve upload tokens
+curl -i -H "Content-Type: application/json" -X GET https://api.figshare.com/v2/account/articles/$id/files?access_token=$token|tee list_files.json
+list_token=($(curl -i -H "Content-Type: application/json" -X GET https://api.figshare.com/v2/account/articles/$id/files?access_token=$token | tr '"' '\n' | grep -E '(^.{8}-)'))
+
+file=fmri_0040048.nii.gz
+upload_token=8a81ae47-df5f-4285-a183-2a81051713e3
+upload_url=https://fup100010.figshare.com/upload/8a81ae47-df5f-4285-a183-2a81051713e3
+id_file=6638550
+curl -i -H "Content-Type: application/json" -X GET $upload_url?access_token=$token
+curl -i -H "Content-Type: application/json" -X PUT --data-binary "@$file" $upload_url/1?access_token=$token
+curl -i -H "Content-Type: application/json" -X POST https://api.figshare.com/v2/account/articles/$id/files/$id_file?access_token=$token 
