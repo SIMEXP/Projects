9112b2c7d72a400a4c9f2be9125c0f6cc43db953
angela-tam <angela.tam08@gmail.com>
2016-11-17 15:13:23 -0500

    xp vbm subtypes and av45 in adni

---
+++ adsf/adsf_xp_dartel_glm_av45_20161110.m
@@ -0,0 +1,27 @@
+clear all
+
+a_path = '/Users/AngelaTam/Desktop/adsf/dartel_subtypes/adni_dartel/dartel_wb_subtypes_20160926/';
+files_in.weight = [a_path 'subtype_weights.mat'];
+files_in.model = '/Users/AngelaTam/Desktop/adsf/dartel_subtypes/adni_dartel/adni_dartel_model_20161110.csv';
+files_out = struct;
+opt.folder_out = [a_path 'glm_av45_coapoe_20161110'];
+psom_mkdir(opt.folder_out)
+opt.scale = 1;
+opt.contrast.AV45 = 1;
+opt.contrast.age = 0;
+opt.contrast.sex = 0;
+opt.contrast.tiv = 0;
+opt.contrast.mean_gm = 0;
+opt.contrast.APOE4 = 0;
+opt.contrast.dx = 0;
+
+niak_brick_association_test(files_in,files_out,opt);
+
+% glm visu
+
+files_in.association = [a_path 'glm_av45_coapoe_20161110/association_stats.mat'];
+% files_in.weight = [a_path 'residual_subtype_weights_20161110.mat']; 
+files_out = struct;
+opt.data_type = 'continuous';
+
+niak_brick_visu_subtype_glm(files_in,files_out,opt);
