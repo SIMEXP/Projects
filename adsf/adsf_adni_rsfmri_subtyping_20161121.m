675f52bf509c106f8be2456936c8746e2f22a038
angela-tam <angela.tam08@gmail.com>
2016-11-23 13:52:59 -0500

    update

--- adsf/adsf_adni_rsfmri_subtyping_20161121.m
+++ adsf/adsf_adni_rsfmri_subtyping_20161121.m
@@ -3,13 +3,13 @@
 clear all
 
 %% set up paths
-path_data = '/gs/project/gsf-624-aa/database2/preventad/results/adni_scores_s007_20160121/rmap_part/';
+path_data = '/home/atam/scratch/rs_subtypes/adni2/rmap_seeds/rmap_stacks/';
 path_sub = '/gs/project/gsf-624-aa/database2/preventad/results/subtype_admci_s07_gui_20160705/';
-path_out = '/home/atam/scratch/rs_subtypes/adni_missing_20161117/';
+path_out = '/home/atam/scratch/rs_subtypes/adni2_subtype_20161123/';
 
 %% set up files_in structure
  
-files_in.model = '/home/atam/scratch/rs_subtypes/model_rs_adni_to_subtype.csv';
+files_in.model = '/home/atam/scratch/rs_subtypes/adni2_model_multi_site_scanner_fd_snr_20161123.csv';
 files_in.mask = '/gs/project/gsf-624-aa/database2/preventad/mask_mnc/mask.mnc';
 files_in.subtype.network_1 = [path_sub 'network_1/network_1_subtype.mat'];
 files_in.subtype.network_2 = [path_sub 'network_2/network_2_subtype.mat'];
@@ -19,46 +19,19 @@ files_in.subtype.network_5 = [path_sub 'network_5/network_5_subtype.mat'];
 files_in.subtype.network_6 = [path_sub 'network_6/network_6_subtype.mat'];
 files_in.subtype.network_7 = [path_sub 'network_7/network_7_subtype.mat'];
 
-%% Configure the inputs for files_in.data
-pheno = niak_read_csv_cell(files_in.model);
 
-% Go through the subjects and then make me some files_in struct
-go_by = '';
-% Find where that is
-for ind = 1:size(pheno,2)
-    if strcmp(pheno{1, ind}, go_by)
-        go_ind = ind;
-    end
-end
 
 %% set up files_in structure
-%for ind = 2:size(pheno,1)
- %   sub_name = [pheno{ind, go_ind}];
-    % Get the file name and path
-  %  file_name = sprintf('smwc1ADNI_%s_MR_MPRAGE.mnc.gz', sub_name);
-   % file_path = [path_data filesep file_name];
-    %files_in.data.(sub_name) = file_path;
-%end
 
 files = dir(path_data);
 files = {files.name};
-
 n_files = length(files);
 
-
-%% set up files_in structure
-for ind = 2:size(pheno,1)
-    for ss = 3:length(files)
-        sub_name = [pheno{ind, go_ind}];
-        % Get the file name and path
-        tmp = strsplit(files{ss},'_');
-        expression = tmp{2};
-        sub_date = tmp{4};
-        matchstr = regexp(sub_name, expression, 'match');
-            if ~isempty(matchstr)
-            files_in.data.(sub_name) = [path_data filesep sprintf('fmri_%s_session1_%s_rmap_part.mnc.gz', sub_name, sub_date)];
-        end
-    end
+for ss = 3:n_files
+    % Get the file name and path
+    tmp = strsplit(files{ss},'_');
+    sub_name = strcat(tmp{1});
+    files_in.data.(sub_name) = [path_data sprintf('%s_stack.mnc.gz', sub_name)];
 end
      
 
