ab6dde6e6dfd4765cf642cacb62a032d80de9c3d
angela-tam <angela.tam08@gmail.com>
2017-03-30 14:37:50 -0400

    updates

--- adsf/adsf_adni2t1_ct_glm_20170303.m
+++ adsf/adsf_adni2t1_ct_glm_20170303.m
@@ -10,7 +10,7 @@ vars = {'diagnosis'};
 
 for ss = 1:length(nb_sbt)
 
-    files_in.weight = strcat(path_c,'wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus/subtype_weights.mat');
+    files_in.weight = strcat(path_c,'wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus/subtype_weights.mat');
     
     for vv = 1:length(vars)
         clear opt
@@ -18,48 +18,13 @@ for ss = 1:length(nb_sbt)
         field = vars{vv};
         
         files_out = struct;
-        opt.folder_out = strcat(path_c,'wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus/glm_nosite/',field,'/');
+        opt.folder_out = strcat(path_c,'wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus/glm/',field,'/');
         psom_mkdir(opt.folder_out);
         opt.scale = 1;
         opt.contrast.age = 0;
         opt.contrast.gender = 0;
         opt.contrast.mean_ct_wb = 0;
-%         opt.contrast.site2 = 0;
-%         opt.contrast.site6 = 0;
-%         opt.contrast.site9 = 0;
-%         opt.contrast.site11 = 0;
-%         opt.contrast.site12 = 0;
-%         opt.contrast.site13 = 0;
-%         opt.contrast.site14 = 0;
-%         opt.contrast.site18 = 0;
-%         opt.contrast.site19 = 0;
-%         opt.contrast.site22 = 0;
-%         opt.contrast.site23 = 0;
-%         opt.contrast.site24 = 0;
-%         opt.contrast.site31 = 0;
-%         opt.contrast.site32 = 0;
-%         opt.contrast.site35 = 0;
-%         opt.contrast.site36 = 0;
-%         opt.contrast.site37 = 0;
-%         opt.contrast.site41 = 0;
-%         opt.contrast.site53 = 0;
-%         opt.contrast.site67 = 0;
-%         opt.contrast.site68 = 0;
-%         opt.contrast.site72 = 0;
-%         opt.contrast.site73 = 0;
-%         opt.contrast.site82 = 0;
-%         opt.contrast.site99 = 0;
-%         opt.contrast.site100 = 0;
-%         opt.contrast.site116 = 0;
-%         opt.contrast.site123 = 0;
-%         opt.contrast.site128 = 0;
-%         opt.contrast.site130 = 0;
-%         opt.contrast.site135 = 0;
-%         opt.contrast.site136 = 0;
-%         opt.contrast.site137 = 0;
-%         opt.contrast.site141 = 0;
-%         opt.contrast.site153 = 0;
-%         opt.contrast.site941 = 0;
+        opt.contrast.manufacturer = 0;
         
         %opt.contrast.diagnosis = 0;
         
@@ -67,7 +32,7 @@ for ss = 1:length(nb_sbt)
         
         niak_brick_association_test(files_in,files_out,opt);
         opt.data_type = 'categorical';
-        files_in.association = strcat(path_c,'wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus/glm_nosite/',field,'/association_stats.mat');
+        files_in.association = strcat(path_c,'wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus/glm/',field,'/association_stats.mat');
         
         adsf_brick_visu_subtype_glm(files_in,files_out,opt);
         
--- adsf/adsf_adni2t1_ct_wb_subs_20170302.m
+++ adsf/adsf_adni2t1_ct_wb_subs_20170302.m
@@ -4,14 +4,14 @@ clear all
 
 path_c = '/Users/AngelaTam/Desktop/adsf/ct_subtypes/';
 
-files_in.data = [path_c 'adni2/adni2_civet_vertex_stack_r_sites_20170301.mat'];
+files_in.data = [path_c 'adni2/adni2_civet_vertex_stack_r_scanner_20170326.mat'];
 files_in.mask = [path_c 'mask_whole_brain.mat'];
 
 nb_sbt = [2 3 4 5 6 7];
 
 for ss = 1:length(nb_sbt)
     files_out = struct;
-    opt.folder_out = strcat(path_c,'adni2/wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus');
+    opt.folder_out = strcat(path_c,'adni2/wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus');
     psom_mkdir(opt.folder_out);
     opt.nb_subtype = nb_sbt(ss);
     
@@ -25,9 +25,9 @@ clear all
 nb_sbt = [2 3 4 5 6 7];
 
 for ss = 1:length(nb_sbt)
-    files_in = strcat('/Users/AngelaTam/Desktop/adsf/ct_subtypes/adni2/wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus/subtype.mat');
+    files_in = strcat('/Users/AngelaTam/Desktop/adsf/ct_subtypes/adni2/wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus/subtype.mat');
     files_out = struct;
-    opt.folder_out = strcat('/Users/AngelaTam/Desktop/adsf/ct_subtypes/adni2/wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus/');
+    opt.folder_out = strcat('/Users/AngelaTam/Desktop/adsf/ct_subtypes/adni2/wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus/');
     opt.nb_subtype = nb_sbt(ss);
     
     adsf_brick_visu_ct_sub(files_in,files_out,opt);
@@ -42,10 +42,10 @@ nb_sbt = [2 3 4 5 6 7];
 
 for ss = 1:length(nb_sbt)
 
-    files_in.data.wb = strcat(path_c,'adni2/adni2_civet_vertex_stack_r_sites_20170301.mat');
-    files_in.subtype.wb = strcat(path_c,'adni2/wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus/subtype.mat');
+    files_in.data.wb = strcat(path_c,'adni2/adni2_civet_vertex_stack_r_scanner_20170326.mat');
+    files_in.subtype.wb = strcat(path_c,'adni2/wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus/subtype.mat');
     files_out = struct;
-    opt.folder_out = strcat(path_c,'adni2/wb_ct_subtypes_20170301_',num2str(nb_sbt(ss)),'clus/');
+    opt.folder_out = strcat(path_c,'adni2/wb_ct_subtypes_scanner/wb_ct_sub_20170326_',num2str(nb_sbt(ss)),'clus/');
     
     adsf_brick_subtype_weight(files_in,files_out,opt);
 
